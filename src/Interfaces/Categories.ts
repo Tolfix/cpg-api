2c76b87ba79bbb144f8aad76238aeadd194281ec
Matteus <tolfx@tolfix.com>
2021-09-15 18:43:58 +0200

    New < Categories.ts >

---
+++ src/Interfaces/Categories.ts
@@ -0,0 +1,17 @@
+import { Document } from "mongoose";
+
+export interface ICategory
+{
+    uid: string;
+    name: string;
+    description: string;
+    private: Boolean;
+}
+
+export interface IDCategory extends Document
+{
+    uid: string;
+    name: string;
+    description: string;
+    private: Boolean;
+}
