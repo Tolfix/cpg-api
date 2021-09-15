4491a358615a5029495043700e78c2a96cb5695d
Matteus <tolfx@tolfix.com>
2021-09-15 19:51:01 +0200

    Auth Method #1

---
+++ src/Middlewares/EnsureAuth.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express"
+import jwt from "jsonwebtoken";
+import { JWT_Access_Token } from "../Config";
+import { APIError } from "../Lib/Response";
+
+export default function EnsureAuth()
+{
+    return (req: Request, res: Response, next: NextFunction) =>
+    {
+        const authHeader = req.headers['authorization'];
+        const token = authHeader;
+        if (token == null)
+            return APIError(`Missing token in headers.`, 401)(res);
+    
+        jwt.verify(token, JWT_Access_Token, (err, payload) =>
+        {
+            if (err) 
+                return APIError(`Unauthorized user.`, 403)(res);
+
+            if(!payload?.data?.personal)
+                return APIError(`Wrong payload.`, 403)(res);
+
+            //@ts-ignore
+            req.payload = payload.data;
+
+            return next();
+        });
+    }
+}
