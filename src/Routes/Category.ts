c1523dcce35fc15620e471d80c5305a7cc5524cc
Matteus <tolfx@tolfix.com>
2021-09-15 18:43:06 +0200

    Modified < Category.ts >

--- src/Routes/Category.ts
+++ src/Routes/Category.ts
@@ -1,15 +1,41 @@
 import { Application, Router } from "express";
+import { CacheCategories } from "../Cache/CacheCategories";
+import Logger from "../Lib/Logger";
+import { APIError, APISuccess } from "../Lib/Response";
 
 export default class CategoryRouter
 {
     private server: Application;
     private router = Router();
-
     public name = "Category";
 
     constructor(server: Application)
     {
         this.server = server;
         this.server.use("/category", this.router);
+
+        this.router.get("/all", (req, res) => {
+            let categories = [];
+
+            for(const [key, value] of CacheCategories.entries())
+            {
+                categories.push(value);
+            }
+
+            APISuccess(categories)(res);
+        });
+
+        this.router.get("/:id", (req, res) => {
+            const id = req.params.id;
+
+            const category = CacheCategories.get(id);
+
+            if(!category)
+                return APIError({
+                    text: `Unable to find category by id ${id}`
+                })(res);
+
+            APISuccess(category)(res);
+        });
     }
 }
