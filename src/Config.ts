8dad488b3221480763f805657fe36b6ebef816c8
Matteus <tolfx@tolfix.com>
2021-09-15 19:49:48 +0200

    Add more configs

--- src/Config.ts
+++ src/Config.ts
@@ -4,7 +4,10 @@
  */
 export const DebugMode = process.env.DEBUG === "true" ? true : false;
 export const HomeDir = ((__dirname.replace("\\build", "")).replace("/build", ""));
+export const JWT_Access_Token = process.env.JWT_ACCESS_TOKEN ?? "";
 
 export const PORT = process.env.PORT ?? 8080;
+
+export const Express_Session_Secret = process.env.SESSION_SECRET ?? require("crypto").randomBytes(20).toString("hex");
 
 export const MongoDB_URI = process.env.MONGO_URI ?? "mongodb://localhost/cpg";
