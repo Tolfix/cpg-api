69a02a63add3eabadca007b2f129506601bcfb06
Matteus <tolfx@tolfix.com>
2021-09-15 17:01:51 +0200

    Modified < Server.ts >

--- src/Server.ts
+++ src/Server.ts
@@ -1,1 +1,27 @@
-export {};
+require("dotenv").config()
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import methodOverride from "method-override";
+import { MongoDB_URI, PORT } from "./Config";
+import Logger from "./Lib/Logger";
+
+const server = express();
+
+mongoose.connect(MongoDB_URI);
+
+server.use(cors({
+    origin: true,
+    credentials: true
+}));
+
+server.use(methodOverride('_method'));
+server.use(express.urlencoded({ extended: true }));
+
+server.use((req, res, next) => {
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader('X-Powered-By', 'Tolfix');
+    next();
+});
+
+const sv = server.listen(PORT, () => Logger.info(`Server listing on port ${PORT}`));
