0038d63e11a057d98d52bd058bcdc991faae14a3
Matteus <tolfx@tolfix.com>
2021-09-15 17:01:44 +0200

    Added more < Configs >

--- src/Config.ts
+++ src/Config.ts
@@ -3,3 +3,7 @@
  * Used if this service is running in debug mode.
  */
 export const DebugMode = process.env.DEBUG === "true" ? true : false;
+
+export const PORT = process.env.PORT ?? 8080;
+
+export const MongoDB_URI = process.env.MONGO_URI ?? "mongodb://localhost/cpg";
