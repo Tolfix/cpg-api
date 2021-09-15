288638a7249ef96fe5ceeb2b1d6a2cb03c85b370
Matteus <tolfx@tolfix.com>
2021-09-15 17:37:38 +0200

    HomeDir added

--- src/Config.ts
+++ src/Config.ts
@@ -3,6 +3,7 @@
  * Used if this service is running in debug mode.
  */
 export const DebugMode = process.env.DEBUG === "true" ? true : false;
+export const HomeDir = ((__dirname.replace("\\build", "")).replace("/build", ""));
 
 export const PORT = process.env.PORT ?? 8080;
 
