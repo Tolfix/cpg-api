bdd7d609b64e21b55b2d38f86769326dbcf9855e
Matteus <tolfx@tolfix.com>
2021-09-15 18:43:39 +0200

    New < Mongo.ts >

---
+++ src/Database/Mongo.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { DebugMode, MongoDB_URI } from "../Config";
+import Logger from "../Lib/Logger";
+
+export default class Mongo_Database
+{
+    private db;
+
+    constructor()
+    {
+        mongoose.connect(MongoDB_URI);
+        
+        this.db = mongoose.connection;
+
+        this.db.on('error', (error: any) => {
+            Logger.error(`A error accured for the database`, error);
+        });
+    
+        this.db.on('disconnected', () => {
+            Logger.error(`Lost connection to the database, shutting down.`);
+            if(!DebugMode)
+                process.exit(1);
+        })
+    
+        this.db.once('open', () => {
+            Logger.info(`Database opened`);
+        });
+    }
+}
