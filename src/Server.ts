6e00ce5170c8a4e54656fe2887291dc7c550fc7a
Matteus <tolfx@tolfix.com>
2021-09-15 18:42:56 +0200

    Modified < Server.ts >

--- src/Server.ts
+++ src/Server.ts
@@ -1,24 +1,21 @@
+/*Copyright (C) 2021 Tolfix - All Rights Reserved*/
 require("dotenv").config()
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
-import methodOverride from "method-override";
-// import swaggerJsDoc, { Options as SwaggerOptions, SwaggerDefinition } from 'swagger-jsdoc';
-// import swaggerUI, { SwaggerUiOptions } from 'swagger-ui-express';
-import { HomeDir, MongoDB_URI, PORT } from "./Config";
+import { PORT } from "./Config";
 import Logger from "./Lib/Logger";
 import RouteHandler from "./Routes/Handler";
+import { reCache } from "./Cache/reCache";
+import Mongo_Database from "./Database/Mongo";
 
 const server = express();
-
-// mongoose.connect(MongoDB_URI);
+new Mongo_Database();
 
 server.use(cors({
     origin: true,
     credentials: true
 }));
 
-server.use(methodOverride('_method'));
 server.use(express.urlencoded({ extended: true }));
 
 server.use((req, res, next) => {
@@ -27,6 +24,8 @@ server.use((req, res, next) => {
     next();
 });
 
-const sv = server.listen(PORT, () => Logger.info(`Server listing on port ${PORT}`));
+reCache().then(() => {
+    RouteHandler(server);
+});
 
-RouteHandler(server);
+const sv = server.listen(PORT, () => Logger.info(`Server listing on port ${PORT}`));
