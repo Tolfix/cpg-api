a9da36626dee17b6d651643935aa01f174c83bb5
Matteus <tolfx@tolfix.com>
2021-09-16 19:31:27 +0200

    Fixed Generator

--- src/Lib/Generator.ts
+++ src/Lib/Generator.ts
@@ -1,12 +1,27 @@
+import { CacheAdmin } from "../Cache/CacheAdmin"
 import { CacheCategories } from "../Cache/CacheCategories"
 import { CacheCustomer } from "../Cache/CacheCustomer"
 
 export function idCustomer()
 {
-    return CacheCustomer.array().length+1
+    let arrayC = CacheCustomer.array();
+    let lastUid = parseInt(arrayC[arrayC.length-1].uid);
+    let uid = lastUid+1;
+    return uid;
 }
 
 export function idCategory()
 {
-    return CacheCategories.array().length+1
+    let arrayC = CacheCategories.array();
+    let lastUid = parseInt(arrayC[arrayC.length-1].uid);
+    let uid = lastUid+1;
+    return uid;
+}
+
+export function idAdmin()
+{
+    let arrayC = CacheAdmin.array();
+    let lastUid = parseInt(arrayC[arrayC.length-1].uid);
+    let uid = lastUid+1;
+    return uid;
 }
