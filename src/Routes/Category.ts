6c0c0174237933fd6b58508cd81466a362aa57e2
Matteus <tolfx@tolfix.com>
2021-09-16 19:31:19 +0200

    Create/Delete < Category >

--- src/Routes/Category.ts
+++ src/Routes/Category.ts
@@ -1,7 +1,11 @@
 import { Application, Router } from "express";
 import { CacheCategories } from "../Cache/CacheCategories";
+import CategoryModel from "../Database/Schemas/Category";
+import AW from "../Lib/AW";
+import { idCategory } from "../Lib/Generator";
 import Logger from "../Lib/Logger";
 import { APIError, APISuccess } from "../Lib/Response";
+import EnsureAdmin from "../Middlewares/EnsureAdmin";
 
 export default class CategoryRouter
 {
@@ -14,11 +18,11 @@ export default class CategoryRouter
         this.server = server;
         this.server.use("/category", this.router);
 
-        this.router.get("/all", (req, res) => {
+        this.router.get("/get/all", (req, res) => {
             APISuccess(CacheCategories.array())(res);
         });
 
-        this.router.get("/:id", (req, res) => {
+        this.router.get("/get/:id", (req, res) => {
             const id = req.params.id;
 
             const category = CacheCategories.get(id);
@@ -29,6 +33,60 @@ export default class CategoryRouter
                 })(res);
 
             APISuccess(category)(res);
+        });
+
+        this.router.post("/post/create", EnsureAdmin, (req, res) => {
+            let { name, description, Private } = req.body;
+
+            if(!name)
+                return APIError({
+                    text: "Missing 'name' in body"
+                })(res);
+
+            if(!description)
+                return APIError({
+                    text: "Missing 'description' in body"
+                })(res);
+
+            if(!Private)
+                Private = false;
+
+            let info = {
+                name,
+                description,
+                private: Private,
+                uid: idCategory().toString(),   
+            }
+
+            new CategoryModel(info).save();
+            CacheCategories.set(info.uid, info);
+
+            APISuccess({
+                text: "Succesfully created category.",
+                uid: info.uid,
+            })(res);
+        });
+
+        this.router.delete("/delete/:uid", EnsureAdmin, async (req, res) => {
+            const uid = req.params.uid;
+
+            if(!CacheCategories.get(uid))
+                return APIError({
+                    text: "Ensure uid is correct",
+                })(res);
+
+            const [Success, Fail] = await AW(CategoryModel.deleteOne({ uid: uid }));
+                
+            if(Fail)
+                return APIError({
+                    text: "Something went wrong, try again later.",
+                })(res);
+
+            CacheCategories.delete(uid);
+            APISuccess({
+                text: "Succesfully deleted category",
+                uid
+            })(res)
         });
     }
 }
