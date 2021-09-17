0fb419e5463bad28e45c4e0e8e26ba02ac883bc1
Matteus <tolfx@tolfix.com>
2021-09-17 20:12:41 +0200

    New < Database/Products >

---
+++ src/Database/Schemas/Products.ts
@@ -0,0 +1,74 @@
+  
+import { model, Schema } from "mongoose"
+import { IDProduct } from "../../Interfaces/Products";
+
+const ProductSchema = new Schema
+(
+    {
+
+        uid: {
+            type: String,
+            required: true,
+        },
+
+        name: {
+            type: String,
+            required: true,
+        },
+
+        description: {
+            type: String,
+            required: true,
+        },
+
+        hidden: {
+            type: Boolean,
+            required: true,
+        },
+
+        category_uid: {
+            type: String,
+            required: true,
+        },
+
+        stock: {
+            type: Number,
+            required: true,
+        },
+
+        BStock: {
+            type: Boolean,
+            required: true,
+        },
+
+        special: {
+            type: Boolean,
+            required: true,
+        },
+
+        payment_type: {
+            type: Object,
+            required: true,
+        },
+
+        price: {
+            type: Number,
+            required: true,
+        },
+
+        setup_fee: {
+            type: Number,
+            required: true,
+        },
+        
+        recurring_method: {
+            type: Object,
+            required: true,
+        },
+
+    }
+);
+
+const ProductModel = model<IDProduct>("products", ProductSchema);
+
+export default ProductModel;
