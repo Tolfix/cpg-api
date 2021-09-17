9c2cdfafe02a44dc9a543dab9dd1ccf95a44f01f
Matteus <tolfx@tolfix.com>
2021-09-17 22:58:56 +0200

    Possible to update product

--- src/Routes/Products.ts
+++ src/Routes/Products.ts
@@ -2,9 +2,11 @@ import { Application, Router } from "express";
 import { CacheProduct } from "../Cache/CacheProduct";
 import ProductModel from "../Database/Schemas/Products";
 import { IProduct } from "../Interfaces/Products";
+import AW from "../Lib/AW";
 import { idProduct } from "../Lib/Generator";
 import { APIError, APISuccess } from "../Lib/Response";
 import EnsureAdmin from "../Middlewares/EnsureAdmin";
+import { isValidProduct } from "../Validator/ValidProducts";
 
 export default class ProductRouter
 {
@@ -58,68 +60,14 @@ export default class ProductRouter
                 stock,
                 BStock
             } = req.body;
+            
 
             if(!product_name)
                 return APIError({
                     text: `Missing 'product_name' in body.`,
                 })(res);
 
-            if(!hidden)
-                hidden = false;
-
-            if(!description)
-                description = '';
-
-            if(!payment_type)
-                payment_type = "free";
-
-            if(payment_type !== "free" || payment_type !== "one_time" || payment_type !== "recurring")
-                return APIError({
-                    text: `'payment_type' didn't contain the following 'free', 'one_time', 'recurring'.`,
-                    valids: ['free', 'one_time', 'recurring']
-                })(res);
-
-            if(!price)
-                price = 0;
-
-            if(typeof price !== "number")
-                return APIError({
-                    text: `'price' has to be a number`,
-                })(res);
-            
-            if(!setup_fee)
-                setup_fee = 0;
-
-            if(typeof setup_fee !== "number")
-                return APIError({
-                    text: `'setup_fee' has to be a number`,
-                })(res);
-
-            if(!special)
-                special = false;
-
-            if(typeof special !== "boolean")
-                return APIError({
-                    text: `'special' has to be a boolean`,
-                })(res);
-
-            if(!stock)
-                stock = 0;
-
-            if(typeof stock !== "number")
-                return APIError({
-                    text: `'stock' has to be a number`,
-                })(res);
-
-            if(!BStock)
-                BStock = false;
-
-            if(typeof BStock !== "boolean")
-                return APIError({
-                    text: `'BStock' has to be a boolean`,
-                })(res);
-
-            const info: IProduct = {
+            let info: IProduct = {
                 uid: idProduct().toString(),
                 category_uid: cUid,
                 description,
@@ -134,18 +82,124 @@ export default class ProductRouter
                 BStock
             };
 
+            if(!hidden)
+                info.hidden = false;
+
+            if(!description)
+                info.description = '';
+
+            if(!payment_type)
+                info.payment_type = "free";
+
+            if(!price)
+                info.price = 0;
+            
+            if(!setup_fee)
+                info.setup_fee = 0;
+
+            if(!special)
+                info.special = false;
+
+            if(!stock)
+                info.stock = 0;
+
+            if(!BStock)
+                info.BStock = false;
+
+            if(!isValidProduct(info, res))
+                return;
+            
             new ProductModel(info).save();
             CacheProduct.set(info.uid, info);
 
-            APISuccess({
+            return APISuccess({
                 text: `Succesfully created product`,
                 uid: info.uid,
                 product: info,
             })(res);
         });
 
-        this.router.patch("/patch/:uid", EnsureAdmin, (req, res) => {
+        this.router.patch("/patch/:uid", EnsureAdmin, async (req, res) => {
+            // Check if product exists
+            const uid = req.params.uid;
+            let product = CacheProduct.get(uid);
 
+            if(!product)
+                return APIError({
+                    text: `Couldn't find product with uid ${uid}`
+                })(res);
+
+            let {
+                description,
+                hidden,
+                payment_type,
+                price,
+                recurring_method,
+                product_name,
+                setup_fee,
+                special,
+                stock,
+                BStock
+            } = req.body;
+
+            let info: IProduct = {
+                uid: uid,
+                category_uid: product.category_uid,
+                description,
+                hidden,
+                name: product_name,
+                payment_type,
+                price,
+                recurring_method,
+                setup_fee,
+                special,
+                stock,
+                BStock
+            };
+
+            if(!isValidProduct(info, res))
+                return;
+
+            if(product_name !== product.name)
+                info.name = product_name;
+
+            if(hidden !== product.hidden)
+                info.hidden = hidden;
+
+            if(description !== product.description)
+                info.description = description;
+
+            if(product.payment_type !== payment_type)
+                info.payment_type = payment_type;
+
+            if(product.price !== price)
+                info.price = price;
+            
+            if(product.setup_fee !== setup_fee)
+                info.setup_fee = setup_fee;
+
+            if(product.special !== special)
+                info.special = special;
+
+            if(product.stock !== stock)
+                info.stock = stock;
+
+            if(product.BStock !== BStock)
+                info.BStock = BStock;
+
+            const [Succes, Fail] = await AW(ProductModel.updateOne({ uid: product.uid }, info));
+
+            if(!Fail)
+                return APIError({
+                    text: `Something went wrong, try again later.`,
+                })(res);
+
+            CacheProduct.set(info.uid, info);
+
+            return APISuccess({
+                text: `Succesfully updated ${info.uid}`,
+                product: info,
+            })(res);
         });
     }
 }
