2d32218e97aec0538baaf5e0a202445ffad4fcf4
Matteus <tolfx@tolfix.com>
2021-09-17 20:12:31 +0200

    New < IProduct >

---
+++ src/Interfaces/Products.ts
@@ -0,0 +1,36 @@
+import { Document } from "mongoose";
+import { ICategory } from "./Categories";
+
+export interface IProduct
+{
+    uid: string;
+    name: string;
+    description: string;
+    category_uid: ICategory["uid"];
+    stock: number;
+    BStock: Boolean;
+    hidden: Boolean;
+    special: number;
+    payment_type: IPaymentType;
+    price: number;
+    setup_fee: number;
+    recurring_method: IRecurringMethod;
+}
+
+export interface IDProduct extends IProduct, Document {};
+
+export interface IPaymentType
+{
+    free: Boolean;
+    one_time: Boolean;
+    recurring: Boolean;
+}
+
+export interface IRecurringMethod
+{
+    monthly: Boolean;
+    quarterly: Boolean;
+    semi_annually: Boolean;
+    biennially: Boolean;
+    triennially: Boolean;
+}
