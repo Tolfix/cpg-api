dfa40c9f0d43360db9cd28bff3da0133c3cdc5c0
Matteus <tolfx@tolfix.com>
2021-09-17 22:04:54 +0200

    Validating types

--- src/Routes/Products.ts
+++ src/Routes/Products.ts
@@ -1,7 +1,7 @@
 import { Application, Router } from "express";
 import { CacheProduct } from "../Cache/CacheProduct";
 import ProductModel from "../Database/Schemas/Products";
-import { IProduct } from "../Interfaces/Products";
+import { IPaymentType, IProduct } from "../Interfaces/Products";
 import { idProduct } from "../Lib/Generator";
 import { APIError, APISuccess } from "../Lib/Response";
 import EnsureAdmin from "../Middlewares/EnsureAdmin";
@@ -59,20 +59,51 @@ export default class ProductRouter
             if(!payment_type)
                 payment_type = "free";
 
+            if(payment_type !== "free" || payment_type !== "one_time" || payment_type !== "recurring")
+                return APIError({
+                    text: `'payment_type' didn't contain the following 'free', 'one_time', 'recurring'.`,
+                    valids: ['free', 'one_time', 'recurring']
+                })(res);
+
             if(!price)
                 price = 0;
+
+            if(typeof price !== "number")
+                return APIError({
+                    text: `'price' has to be a number`,
+                })(res);
             
             if(!setup_fee)
                 setup_fee = 0;
 
+            if(typeof setup_fee !== "number")
+                return APIError({
+                    text: `'setup_fee' has to be a number`,
+                })(res);
+
             if(!special)
                 special = false;
+
+            if(typeof special !== "boolean")
+                return APIError({
+                    text: `'special' has to be a boolean`,
+                })(res);
 
             if(!stock)
                 stock = 0;
 
+            if(typeof stock !== "number")
+                return APIError({
+                    text: `'stock' has to be a number`,
+                })(res);
+
             if(!BStock)
                 BStock = false;
+
+            if(typeof BStock !== "boolean")
+                return APIError({
+                    text: `'BStock' has to be a boolean`,
+                })(res);
 
             const info: IProduct = {
                 uid: idProduct().toString(),
