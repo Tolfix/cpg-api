db1caa397ce25f13fc1892a885745db4b6e361d7
Matteus <tolfx@tolfix.com>
2021-09-17 22:17:27 +0200

    more recaching

--- src/Cache/reCache.ts
+++ src/Cache/reCache.ts
@@ -1,9 +1,13 @@
 import { DebugMode } from "../Config";
 import AdminModel from "../Database/Schemas/Administrators";
 import CategoryModel from "../Database/Schemas/Category";
+import CustomerModel from "../Database/Schemas/Customer";
+import ProductModel from "../Database/Schemas/Products";
 import Logger from "../Lib/Logger";
 import { CacheAdmin } from "./CacheAdmin";
 import { CacheCategories } from "./CacheCategories";
+import { CacheCustomer } from "./CacheCustomer";
+import { CacheProduct } from "./CacheProduct";
 
 export function reCache_Categories()
 {
@@ -22,7 +26,7 @@ export function reCache_Categories()
             uid: "1",
         });
     }
-
+    Logger.info(`Starting caching on categories..`);
     return new Promise(async (resolve, reject) => {
         const categories = await CategoryModel.find();
         for (const c of categories)
@@ -36,6 +40,7 @@ export function reCache_Categories()
 
 export async function reCache_Admin()
 {
+    Logger.info(`Starting caching on admins..`);
     return new Promise(async (resolve, reject) => {
         const admin = await AdminModel.find();
         for (const a of admin)
@@ -47,8 +52,38 @@ export async function reCache_Admin()
     });
 }
 
+export async function reCache_Customers()
+{
+    Logger.info(`Starting caching on customers..`);
+    return new Promise(async (resolve, reject) => {
+        const customer = await CustomerModel.find();
+        for (const c of customer)
+        {
+            Logger.cache(`Caching customer ${c.uid}`);
+            CacheCustomer.set(c.uid, c);
+        }
+        return resolve(true);
+    });
+}
+
+export async function reCache_Product()
+{
+    Logger.info(`Starting caching on products..`);
+    return new Promise(async (resolve, reject) => {
+        const product = await ProductModel.find();
+        for (const c of product)
+        {
+            Logger.cache(`Caching product ${c.uid}`);
+            CacheProduct.set(c.uid, c);
+        }
+        return resolve(true);
+    });
+}
+
 export async function reCache()
 {
     await reCache_Categories();
     await reCache_Admin();
+    await reCache_Customers();
+    await reCache_Product();
 }
