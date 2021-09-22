228661262267f415130200c9f9634cf2b59a4c95
Matteus <tolfx@tolfix.com>
2021-09-22 21:12:58 +0200

    New routes for invoices

--- src/Routes/Invoices.ts
+++ src/Routes/Invoices.ts
@@ -1,4 +1,5 @@
 import { Application, Router } from "express";
+import { CacheInvoice } from "../Cache/CacheInvoices";
 import Logger from "../Lib/Logger";
 import { APIError, APISuccess } from "../Lib/Response";
 import EnsureAdmin from "../Middlewares/EnsureAdmin";
@@ -12,6 +13,27 @@ export default class InvoiceRouter
     {
         this.server = server;
         this.server.use("/invoice", this.router);
+
+        this.router.get("/get/all", EnsureAdmin, (req, res) => {
+            APISuccess({
+                invoices: CacheInvoice.array(),
+            })(res);
+        });
+
+        this.router.get("/get/:uid", EnsureAdmin, (req, res) => {
+            const uid = req.params.uid;
+
+            const invoice = CacheInvoice.get(uid);
+
+            if(!invoice)
+                return APIError({
+                    text: `Unable to find invoice by uid ${uid}`,
+                })(res);
+
+            return APISuccess({
+                invoice: invoice,
+            })(res);
+        });
 
     }
 }
