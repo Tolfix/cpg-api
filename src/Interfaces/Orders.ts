00f8e65a6efed037f4b0f058c1da5024afd11169
Matteus <tolfx@tolfix.com>
2021-09-22 20:12:56 +0200

    Modified < IOrders >

--- src/Interfaces/Orders.ts
+++ src/Interfaces/Orders.ts
@@ -3,12 +3,17 @@ import { IPaymentType, IProduct, IRecurringMethod } from "./Products";
 
 export interface IOrder
 {
+    uid: string;
+    invoice_uid?: any;
     customer_uid: ICustomer["uid"];
     payment_method: any;
     order_status: OrderStatus;
-
     product_uid: IProduct["uid"];
     billing_type: IPaymentType;
+    /**
+     * @description
+     * if 'billing_type' is recurring billing_cycle wont be optional
+     */
     billing_cycle?: IRecurringMethod;
     quantity: number;
     price_override: number;
