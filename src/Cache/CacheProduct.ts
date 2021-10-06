b6e4d20dde86d8fe41035ad9247b9bf5b751447b
Tolfx <matteus@tolfx.dev>
2021-10-06 13:56:35 +0200

    New method

--- src/Cache/CacheProduct.ts
+++ src/Cache/CacheProduct.ts
@@ -1,3 +1,9 @@
+import { ICategory } from "../Interfaces/Categories";
 import { IProduct } from "../Interfaces/Products";
 
 export const CacheProduct = new Map<IProduct["uid"], IProduct>();
+
+export function getProductByCategoryUid(uid: ICategory["uid"])
+{
+    return CacheProduct.array().filter(e => e.category_uid === uid);
+}
