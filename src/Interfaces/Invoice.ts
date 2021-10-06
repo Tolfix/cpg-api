418de1157b66037246490a88bf2ce20b994d6984
Matteus <tolfx@tolfix.com>
2021-10-06 18:52:08 +0200

    Small changes for Transactions

--- src/Interfaces/Invoice.ts
+++ src/Interfaces/Invoice.ts
@@ -2,6 +2,7 @@ import { Document } from "mongoose";
 import { ICustomer } from "./Customer";
 import { OrderStatus } from "./Orders";
 import { IPayments } from "./Payments";
+import { ITransactions } from "./Transactions";
 
 export interface IInvoice
 {
@@ -11,7 +12,7 @@ export interface IInvoice
     dates: IInvoice_Dates;
     amount: number;
     items: Array<IInvoices_Items>;
-    transactions?: Array<IInvoices_Transactions>;
+    transactions?: Array<ITransactions>;
     payment_method: keyof IPayments;
     status: OrderStatus | "draft" | "refunded" | "collections" | "payment_pending";
     tax_rate: number;
@@ -32,13 +33,4 @@ export interface IInvoices_Items
     notes: string;
     amount: IInvoice["amount"];
     taxed: Boolean;
-}
-
-export interface IInvoices_Transactions
-{
-    uid: string;
-    date: string;
-    payment_method: IInvoice["payment_method"];
-    amount: IInvoice["amount"];
-    fees: number;
 }
