3fcac0e6b7e5690d7c451859315c56407d50ea9d
Matteus <tolfx@tolfix.com>
2021-10-11 20:34:25 +0200

    Create invoice

--- src/Routes/Invoices.ts
+++ src/Routes/Invoices.ts
@@ -1,9 +1,14 @@
 import { Application, Router } from "express";
+import { CacheCustomer } from "../Cache/CacheCustomer";
 import { CacheInvoice } from "../Cache/CacheInvoices";
+import InvoiceModel from "../Database/Schemas/Invoices";
+import { ICustomer } from "../Interfaces/Customer";
 import { IInvoice } from "../Interfaces/Invoice";
+import { idInvoice } from "../Lib/Generator";
 import Logger from "../Lib/Logger";
 import { APIError, APISuccess } from "../Lib/Response";
 import EnsureAdmin from "../Middlewares/EnsureAdmin";
+import { isValidInvoice } from "../Validator/ValidInvoice";
 
 export default class InvoiceRouter
 {
@@ -14,6 +19,7 @@ export default class InvoiceRouter
     {
         this.server = server;
         this.server.use("/invoices", this.router);
+
 
         /**
          * Gets all invoices
@@ -51,6 +57,71 @@ export default class InvoiceRouter
 
             return APISuccess({
                 invoice: invoice,
+            })(res);
+        });
+
+        /**
+         * Creates a invoice
+         * @route POST /invoices/create
+         * @group Invoices
+         * @param {string} customer_uid.query.required - uid for customer.
+         * @param {InvoiceCreate.model} data.body.required - Body for invoice.
+         * @returns {Object} 200 - Created a new product.
+         * @returns {Error} default - Missing something
+         * @security JWT
+         * @security Basic
+         */
+        this.router.post("/create", EnsureAdmin, (req, res) => {
+
+            const customer_uid = req.query.customer_uid as ICustomer["uid"];
+
+            if(!customer_uid)
+                return APIError({
+                    text: "No 'customer_uid' in query'"
+                })(res);
+
+            if(!CacheCustomer.get(customer_uid))
+                return APIError({
+                    text: `Unable to find customer by ${customer_uid}`,
+                })(res);
+
+            let {
+                amount,
+                items,
+                transactions,
+                payment_method,
+                dates,
+                status,
+                tax_rate,
+                notes,
+                paid,
+            } = req.body as any;
+
+            let info: IInvoice = {
+                uid: idInvoice(),
+                customer_uid: customer_uid,
+                invoiced_to: customer_uid,
+                dates,
+                status,
+                tax_rate,
+                notes,
+                paid,
+                amount,
+                payment_method,
+                items,
+                transactions
+            }
+
+            if(!isValidInvoice(info, res))
+                return;
+
+            new InvoiceModel(info).save();
+            CacheInvoice.set(info.uid, info);
+
+            return APISuccess({
+                text: `Succesfully created invoice`,
+                uid: info.uid,
+                invoice: info,
             })(res);
         });
 
