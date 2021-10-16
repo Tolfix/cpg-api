7e0b8644e99ca1134699e2d321cf629e1b857f29
Matteus <tolfx@tolfix.com>
2021-10-16 18:13:02 +0200

    Modified route handler

--- src/Handlers/Route.ts
+++ src/Handlers/Route.ts
@@ -15,8 +15,9 @@ export default function RouteHandler(server: Application): void
     readdirSync(`${routeDir}`).forEach((dir) => {
         const routes = readdirSync(`${routeDir}/${dir}`).filter((f) => f.endsWith('.js'));
         for (let file of routes) {
-            const pull = require(`${routeDir}/${file}`).default;
-            if (pull) {
+            const pull = require(`${routeDir}/${dir}/${file}`).default;
+            if (pull)
+            {
                 Logger.info(`Adding new router ${pull.name ?? ""}`)
                 new pull(server);
             }
