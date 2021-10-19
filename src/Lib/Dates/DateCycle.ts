239c4dbab16bbd6a1ed0f37c0c93d84daab1ac60
Matteus <tolfx@tolfix.com>
2021-10-19 20:25:31 +0200

    New < DateCycle.ts >

---
+++ src/Lib/Dates/DateCycle.ts
@@ -0,0 +1,22 @@
+import { IRecurringMethod } from "../../Interfaces/Products";
+import datetime from "date-and-time";
+
+export default function nextRycleDate(date: Date, type: IRecurringMethod): Date
+{
+    if(type === "monthly")
+        return (datetime.addMonths(date, 1));
+
+    if(type === "biennially")
+        return (datetime.addYears(date, 2));
+    
+    if(type === "quarterly")
+        return (datetime.addMonths(date, 4));
+
+    if(type === "semi_annually")
+        return (datetime.addMonths(date, 6));
+
+    if(type === "triennially")
+        return (datetime.addYears(date, 3));
+
+    return (datetime.addMonths(date, 1));
+}
