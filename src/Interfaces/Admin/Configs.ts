0209835eca9267b284233ce3132d865736906b37
Matteus <tolfx@tolfix.com>
2021-10-19 20:25:54 +0200

    Config for emails #7

---
+++ src/Interfaces/Admin/Configs.ts
@@ -0,0 +1,18 @@
+import { Document } from "mongoose";
+
+export interface IConfigs
+{
+    smtp: ISmtp;
+    smtp_emails: Array<string>;
+}
+
+export interface IDConfigs extends IConfigs, Document {};
+
+export interface ISmtp
+{
+    host: string;
+    username: string;
+    password: string;
+    secure: Boolean;
+    port: number;
+}
