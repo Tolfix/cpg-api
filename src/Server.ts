09ad6312e86ade4bd2176082153ec3f98e95bd28
Matteus <tolfx@tolfix.com>
2021-10-30 19:13:55 +0200

    Small changes

--- src/Server.ts
+++ src/Server.ts
@@ -1,7 +1,3 @@
-/* Copyright (C) 2021 Tolfix - All Rights Reserved */
-require("dotenv").config();
-require("./Mods/MapMod");
-require("./Events/NodeEvents");
 import express from "express";
 import cors from "cors";
 import session from "express-session";
@@ -10,7 +6,6 @@ import { Express_Session_Secret, HomeDir, PORT } from "./Config";
 import Logger from "./Lib/Logger";
 import RouteHandler from "./Handlers/Route";
 import { reCache } from "./Cache/reCache";
-import Mongo_Database from "./Database/Mongo";
 import { ICustomer } from "./Interfaces/Customer";
 import AdminHandler from "./Admin/AdminHandler";
 import { APIError } from "./Lib/Response";
@@ -21,47 +16,48 @@ declare module "express-session"
         payload?: ICustomer;
     }
 }
+export default function start()
+{
 
-const server = express();
-new Mongo_Database();
-new AdminHandler();
-
-server.use(fileUpload({
-    createParentPath: true,
-}));
-
-let sessionMiddleWare = session({
-    secret: Express_Session_Secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        path: "/",
-        maxAge: 30*24*60*60*1000,
-    }
-});
-
-server.use(sessionMiddleWare);
-
-server.use(cors({
-    origin: true,
-    credentials: true
-}));
-
-server.use(express.urlencoded({ extended: true }));
-server.use(express.json());
-
-server.use((req, res, next) => {
-    // res.setHeader("Content-Type", "application/json");
-    res.setHeader('X-Powered-By', 'Tolfix');
-    next();
-});
-
-reCache().then(() => {
-    RouteHandler(server);
-    const sv = server.listen(PORT, () => Logger.info(`Server listing on port ${PORT}`));
-    server.use("*", (req, res) => {
-        return APIError({
-            text: `Couldn't find what you were looking for.`
-        })(res);
-    })
-});
+    const server = express();
+        
+    server.use(fileUpload({
+        createParentPath: true,
+    }));
+    
+    let sessionMiddleWare = session({
+        secret: Express_Session_Secret,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            path: "/",
+            maxAge: 30*24*60*60*1000,
+        }
+    });
+    
+    server.use(sessionMiddleWare);
+    
+    server.use(cors({
+        origin: true,
+        credentials: true
+    }));
+    
+    server.use(express.urlencoded({ extended: true }));
+    server.use(express.json());
+    
+    server.use((req, res, next) => {
+        // res.setHeader("Content-Type", "application/json");
+        res.setHeader('X-Powered-By', 'Tolfix');
+        next();
+    });
+    
+    reCache().then(() => {
+        RouteHandler(server);
+        const sv = server.listen(PORT, () => Logger.info(`Server listing on port ${PORT}`));
+        server.use("*", (req, res) => {
+            return APIError({
+                text: `Couldn't find what you were looking for.`
+            })(res);
+        })
+    });
+}
