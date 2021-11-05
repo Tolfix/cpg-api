596d94f1ea1d0e668d6dabe2cf6daab0c2e1b4c1
Matteus <tolfx@tolfix.com>
2021-11-05 13:12:35 +0100

    Modified invoices

--- src/Routes/v2/Invoices/Invoices.config.ts
+++ src/Routes/v2/Invoices/Invoices.config.ts
@@ -63,7 +63,7 @@ export default class InvoiceRouter
                 "marginRight": 25,
                 "marginLeft": 25,
                 "marginBottom": 25,
-                "logo": "https://cdn.tolfix.com/images/TX-Small.png", //or base64
+                "logo": "https://cdn.tolfix.com/images/Tolfix.png", //or base64
                 "sender": {
                     "company": "Tolfix",
                     "address": "Kalendervägen 23",
