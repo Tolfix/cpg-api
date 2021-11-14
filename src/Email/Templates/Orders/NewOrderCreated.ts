845db059e0c796bfa51680bea7ac4b8d8ec322d5
Matteus <tolfx@tolfix.com>
2021-11-14 17:02:31 +0100

    Template for emails

---
+++ src/Email/Templates/Orders/NewOrderCreated.ts
@@ -0,0 +1,22 @@
+import { ICustomer } from "../../../Interfaces/Customer";
+import { IOrder } from "../../../Interfaces/Orders";
+import getFullName from "../../../Lib/Customers/getFullName";
+import getProductById from "../../../Lib/Products/getProductById";
+
+export default (order: IOrder, customer: ICustomer) => `Hello ${getFullName(customer)}!
+<br />
+A new order has been created for you.
+<br />
+<br />
+
+Order nmr.: ${order.id}
+<br />
+Products: ${order.products_uid.map(async product => {
+    const p = await getProductById(product as any);
+    if(!p)
+        return '';
+    return `${p.name} - (${p.price} SEK)`;
+}).join("<br />")}
+<br />
+
+`
