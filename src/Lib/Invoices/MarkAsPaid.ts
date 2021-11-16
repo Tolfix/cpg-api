548976f1bd5cb524a8c0c15b1d0e8a62736f541f
Matteus <tolfx@tolfix.com>
2021-11-16 21:37:55 +0100

    New < MarkAsPaid.ts >

---
+++ src/Lib/Invoices/MarkAsPaid.ts
@@ -0,0 +1,16 @@
+import InvoiceModel from "../../Database/Schemas/Invoices";
+import { IInvoice } from "../../Interfaces/Invoice";
+import { Document } from "mongoose";
+
+export async function getInvoiceByIdAndMarkAsPaid(id: number | string): Promise<IInvoice & Document>
+{
+    return new Promise(async (resolve, reject) => {
+        const invoice = await InvoiceModel.findOne({ id: id });
+        if(!invoice)
+            return reject("Unable to find invoice");
+    
+        invoice.paid = true;
+        await invoice.save();
+        return resolve(invoice);
+    }) 
+}
