c9472a04dd70391f4edd6b93f7bdf52ed09db340
Matteus <tolfx@tolfix.com>
2021-11-17 18:55:01 +0100

    Modified cron invoices

--- src/Cron/CronInvoices.ts
+++ src/Cron/CronInvoices.ts
@@ -3,9 +3,8 @@ import InvoiceModel from "../Database/Schemas/Invoices";
 import Logger from "../Lib/Logger";
 import dateFormat from "date-and-time";
 import CustomerModel from "../Database/Schemas/Customer";
-import { SendEmail } from "../Email/Send";
-import createPDFInvoice from "../Lib/Invoices/CreatePDFInvoice";
-import { d_Days, Full_Domain } from "../Config";
+import { d_Days } from "../Config";
+import { sendInvoiceEmail } from "../Lib/Invoices/SendEmail";
 
 export default function Cron_Invoices()
 {
@@ -36,45 +35,7 @@ export default function Cron_Invoices()
                 if(!Customer)
                     continue;
 
-                if(!Customer.personal.email)
-                    continue;
-                
-                //@ts-ignore
-                SendEmail(Customer.personal.email, `Invoice From Tolfix ${invoice.id}`, {
-                    isHTML: true,
-                    attachments: [
-                        {
-                            filename: 'invoice.pdf',
-                            content: Buffer.from(await createPDFInvoice(invoice) ?? "==", 'base64'),
-                            contentType: 'application/pdf'
-                        }
-                    ],
-                    body: `Hello ${Customer.personal.first_name} ${Customer.personal.last_name} <br />
-                    A gentle reminder you have a invoice due to <strong>${invoice.dates.due_date}</strong> <br />
-                    <br />
-                    Reference invoice id <strong>INVOICE ${invoice.id}</strong> when paying!
-                    <br />
-                    <br />
-                    Payment method: ${invoice.payment_method}
-                    ${invoice.payment_method === "paypal" ? `<br />
-                    <a href="${Full_Domain}/v2/paypal/pay/${invoice.uid}" target="_blank">
-                        Click me to pay.
-                    </a>
-                    ` : ''}
-                    <br />
-                    <br />
-                    Company information : <a href="https://tolfix.com/knowledgebase">https://tolfix.com/knowledgebase</a>
-                    <br />
-                    Company Billing : <a href="https://tolfix.com/about/billing">https://tolfix.com/about/billing</a>
-                    `
-                }, (err, sent) => {
-                    if(!err && sent)
-                    {
-                        invoice.notified = true;
-                        invoice.status = "payment_pending";
-                        invoice.save();
-                    }
-                })
+                await sendInvoiceEmail(invoice, Customer);
 
             }
         });
