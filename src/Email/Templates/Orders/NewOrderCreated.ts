f9b7df4050db1b6bfa5df0d59b5647ec1931afda
Matteus <tolfx@tolfix.com>
2021-11-18 21:48:42 +0100

    Modified new order template email

--- src/Email/Templates/Orders/NewOrderCreated.ts
+++ src/Email/Templates/Orders/NewOrderCreated.ts
@@ -2,21 +2,26 @@ import { ICustomer } from "../../../Interfaces/Customer";
 import { IOrder } from "../../../Interfaces/Orders";
 import getFullName from "../../../Lib/Customers/getFullName";
 import getProductById from "../../../Lib/Products/getProductById";
+import Footer from "../General/Footer";
 
-export default (order: IOrder, customer: ICustomer) => `Hello ${getFullName(customer)}!
+export default async (order: IOrder, customer: ICustomer) => `Hello ${getFullName(customer)}!
 <br />
-A new order has been created for you.
-<br />
-<br />
-
 Order nmr.: ${order.id}
 <br />
-Products: ${order.products.map(async product => {
+Products: ${(await Promise.all(order.products.map(async (product) => {
     const p = await getProductById(product.product_id as any);
-    if(!p)
+    if (!p)
         return '';
     return `${p.name} - (${p.price} SEK)`;
-}).join("<br />")}
+}))).join("<br />")}
 <br />
+Total: ${(await Promise.all(order.products.map(async (product) => {
+    const p = await getProductById(product.product_id as any);
+    if (!p)
+        return 0;
+    return p.price;
+}))).reduce((acc, cur) => acc + cur, 0)} SEK
 
+<br />
+${Footer}
 `
