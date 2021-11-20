37dc6f3aa603edd4ff831ecfb6caafef2ef6fe62
Matteus <tolfx@tolfix.com>
2021-11-21 00:59:08 +0100

    [Feature] Stripe #19

--- src/Payments/Stripe.ts
+++ src/Payments/Stripe.ts
@@ -1,30 +1,65 @@
 import stripe from "stripe";
 import { DebugMode, Stripe_SK_Live, Stripe_SK_Test } from "../Config";
 import CustomerModel from "../Database/Schemas/Customer";
-import ProductModel from "../Database/Schemas/Products";
-import { IOrder } from "../Interfaces/Orders";
+import InvoiceModel from "../Database/Schemas/Invoices";
+import TransactionsModel from "../Database/Schemas/Transactions";
+import { IInvoice } from "../Interfaces/Invoice";
+import { idTransicitons } from "../Lib/Generator";
+import { getInvoiceByIdAndMarkAsPaid } from "../Lib/Invoices/MarkAsPaid";
+import { getDate } from "../Lib/Time";
 const Stripe = new stripe(DebugMode ? Stripe_SK_Test : Stripe_SK_Live, {
     apiVersion: "2020-08-27",
 });
 
-// Create a method that will create a payment intent from an order
-export const CreatePaymentIntent = async (order: IOrder) => {
-    const customer = await CustomerModel.findOne({ id: order.customer_uid });
-    const Products = await ProductModel.find({ id: {
-        $in: [...order.products.map(product => product.product_id)]
-    } });
+const cacheIntents = new Map<string, stripe.Response<stripe.PaymentIntent>>();
 
-    return (await Stripe.paymentIntents.create({
-        amount: order.price_override ? order.price_override : Products.reduce((acc, cur) => acc + cur.price, 0) ?? 0 * 100,
-        currency: "usd",
+// Create a method that will create a payment intent from an order
+export const CreatePaymentIntent = async (invoice: IInvoice) =>
+{
+    if(cacheIntents.has(invoice.uid))
+        return cacheIntents.get(invoice.uid) as stripe.Response<stripe.PaymentIntent>;
+
+    const customer = await CustomerModel.findOne({ id: invoice.customer_uid });
+
+    let intent = (await Stripe.paymentIntents.create({
+        amount: (invoice.amount+invoice.amount*invoice.tax_rate/100) * 100,
+        currency: "sek",
         payment_method_types: ["card"],
         receipt_email: customer?.personal.email,
         // @ts-ignore
-        description: "Order #" + order.id,
+        description: "Invoice #" + invoice.id,
         metadata: {
             // @ts-ignore
-            order_id: order.id,
-            order_uid: order.uid,
+            invoice_id: invoice.id,
+            invoice_uid: invoice.uid,
         },
     }));
+
+    cacheIntents.set(invoice.uid, intent);
+
+    return intent;
 };
+
+export const RetrivePaymentIntent = async (payment_intent: string) =>
+{
+    return (await Stripe.paymentIntents.retrieve(payment_intent));
+}
+
+export const markInvoicePaid = async (intent: stripe.Response<stripe.PaymentIntent>) =>
+{
+    const invoice = await getInvoiceByIdAndMarkAsPaid(intent.metadata.invoice_id);
+
+    const newTrans = await (new TransactionsModel({
+        amount: invoice.amount,
+        payment_method: invoice.payment_method,
+        fees: 0,
+        invoice_uid: invoice.id,
+        customer_uid: invoice.customer_uid,
+        date: getDate(),
+        uid: idTransicitons(),
+    }).save());
+
+    invoice?.transactions.push(newTrans.id);
+
+    await invoice.save();
+}
--- src/Routes/v2/Stripe/Stripe.config.ts
+++ src/Routes/v2/Stripe/Stripe.config.ts
@@ -1,4 +1,12 @@
 import { Application, Router } from "express";
+import { DebugMode, Full_Domain, Stripe_PK_Public, Stripe_PK_Public_Test, Stripe_SK_Live, Stripe_SK_Test, Stripe_Webhook_Secret } from "../../../Config";
+import InvoiceModel from "../../../Database/Schemas/Invoices";
+import { APIError } from "../../../Lib/Response";
+import stripe from "stripe";
+import { CreatePaymentIntent, markInvoicePaid, RetrivePaymentIntent } from "../../../Payments/Stripe";
+const Stripe = new stripe(DebugMode ? Stripe_SK_Test : Stripe_SK_Live, {
+    apiVersion: "2020-08-27",
+});
 
 export default class StripeRouter
 {
@@ -10,7 +18,288 @@ export default class StripeRouter
         this.server = server;
         this.server.use(`/${version}/stripe`, this.router);
 
+        this.router.get("/pay/:invoiceId", async (req, res) => {
+            const invoiceId = req.params.invoiceId;
+            const invoice = await InvoiceModel.findOne( { id: invoiceId } );
+            if(!invoice)
+                return APIError("Couldn't find invoice")(res);
+
+            if(invoice.paid)
+                return APIError("Invoice already paid")(res);
+
+            const intent = await CreatePaymentIntent(invoice);
+
+            res.send(`
+            <head>
+                <title>Checkout | ${invoice.id}</title>
+                <script src="https://js.stripe.com/v3/"></script>
+                <style>
+                    * {
+                        font-family: "Verdana";
+                    }
+                    table, td, th {  
+                        border: 1px solid #ddd;
+                        text-align: left;
+                    }
+                    
+                    table {
+                        border-collapse: collapse;
+                        width: 100%;
+                    }
+                    
+                    th, td {
+                        padding: 15px;
+                    }
+                    body {
+                        text-align: center;
+                        padding: 40px 0;
+                        background: #EBF0F5;
+                    }
+                    .card {
+                        background: white;
+                        padding: 40px;
+                        border-radius: 4px;
+                        box-shadow: 0 2px 3px #C8D0D8;
+                        margin: 20px;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="card">
+                    <div id="items">
+
+                        <!-- Table of items from invoice -->
+                        <table>
+                            <tr>
+                                <th>Item</th>
+                                <th>Price</th>
+                            </tr>
+                            ${invoice.items.map(item => `
+                                <tr>
+                                    <td>${item.notes}</td>
+                                    <td>${item.amount} SEK</td>
+                                </tr>
+                            `).join("")}
+
+                            <tr>
+                                <td>Tax</td>
+                                <td>${invoice.tax_rate}%</td>
+                            </tr>
+                            <tr>
+                                <td>Total</td>
+                                <td>${invoice.amount+invoice.amount*invoice.tax_rate/100} SEK</td>
+                            </tr>
+                        </table>
+
+
+                    </div>
+                    <hr />
+                    <form id="payment-form">
+                        <div id="payment-element">
+                            <!-- Elements will create form elements here -->
+                        </div>
+                        <button style="
+                            padding: 20px;
+                            margin-top: 30px;
+                            display: block;
+                            width: 100%;
+                            " id="submit">Submit</button>
+                        <div style="
+                        margin-top: 10px;
+                        " id="error-message">
+                            <!-- Display error message to your customers here -->
+                        </div>
+                    </form>
+                </div>
+
+                <script>
+                    const stripe = Stripe('${DebugMode ? Stripe_PK_Public_Test : Stripe_PK_Public}');
+                    
+                    const appearance = {
+                        theme: 'none',
+                        variables: {
+                          fontFamily: 'Verdana',
+                          fontLineHeight: '1.5',
+                          borderRadius: '0',
+                          colorBackground: '#dfdfdf'
+                        },
+                        rules: {
+                          '.Input': {
+                            backgroundColor: '#ffffff',
+                            boxShadow: 'inset -1px -1px #ffffff, inset 1px 1px #0a0a0a, inset -2px -2px #dfdfdf, inset 2px 2px #808080'
+                          },
+                          '.Input--invalid': {
+                            color: '#DF1B41'
+                          },
+                          '.Tab, .Block': {
+                            boxShadow: 'inset -1px -1px #0a0a0a, inset 1px 1px #ffffff, inset -2px -2px #808080, inset 2px 2px #dfdfdf'
+                          },
+                          '.Tab:hover': {
+                            backgroundColor: '#eee'
+                          },
+                          '.Tab--selected, .Tab--selected:focus, .Tab--selected:hover': {
+                            backgroundColor: '#ccc'
+                          }
+                        }
+                      };
+                    
+                    const options = {
+                        clientSecret: '${intent.client_secret}',
+                        appearance: appearance,
+                    };
+                      
+                    // Set up Stripe.js and Elements to use in checkout form, passing the client secret obtained in step 2
+                    const elements = stripe.elements(options);
+                    
+                    // Create and mount the Payment Element
+                    const paymentElement = elements.create('payment');
+                    paymentElement.mount('#payment-element');
+                </script>
+
+                <script>
+                    const form = document.getElementById('payment-form');
+
+                    form.addEventListener('submit', async (event) => {
+                    event.preventDefault();
+                    
+                    const {error} = await stripe.confirmPayment({
+                        elements,
+                        confirmParams: {
+                            return_url: '${Full_Domain}/v2/stripe/${invoice.id}/complete',
+                        },
+                    });
+                    
+                    if (error) {
+                        const messageContainer = document.querySelector('#error-message');
+                        messageContainer.textContent = error.message;
+                    } else {
+
+                    }
+                    });
+                </script>
+
+            </body>
+            `)
+        });
+
+        this.router.get("/:invoiceId/complete", async (req, res) => {
+            const invoiceId = req.params.invoiceId;
+            const payment_intent = req.query.payment_intent as string;
+            const invoice = await InvoiceModel.findOne( { id: invoiceId } );
+            if(!invoice)
+                return APIError("Couldn't find invoice")(res);
+
+            if(!payment_intent)
+                return APIError("No payment_intent")(res);
+
+            const intent = await RetrivePaymentIntent(payment_intent);
+
+            let message = "";
+            let href = "";
+            let status = "";
+
+            switch (intent.status) {
+                case 'succeeded':
+                    message = 'Success! Payment received.';
+                    invoice.paid = true;
+                    href = "https://tolfix.com/";
+                    status = intent.status;
+                    break;
+            
+                case 'processing':
+                    message = "Payment processing. We'll update you when payment is received.";
+                    href = "https://tolfix.com/";
+                    status = intent.status;
+                    break;
+            
+                case 'requires_payment_method':
+                    message = 'Payment failed. Please try another payment method.';
+                    href = `${Full_Domain}/v2/stripe/pay/${invoice.id}`;
+                    status = intent.status;
+                    // Redirect your user back to your payment page to attempt collecting
+                    // payment again
+                    break;
+            
+                default:
+                    message = 'Something went wrong.';
+                    href = "https://tolfix.com/";
+                    break;
+            }
+
+            res.send(`
+            <html>
+                <head>
+                    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
+                </head>
+                <style>
+                    body {
+                        text-align: center;
+                        padding: 40px 0;
+                        background: #EBF0F5;
+                    }
+                    h1 {
+                        color: ${status === "requires_payment_method" ? '#B04B4B' : '#88B04B'};
+                        font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+                        font-weight: 900;
+                        font-size: 40px;
+                        margin-bottom: 10px;
+                    }
+                    i {
+                        color: ${status === "requires_payment_method" ? '#B04B4B' : '#9ABC66'};
+                        font-size: 100px;
+                        line-height: 200px;
+                        margin-left:-15px;
+                    }
+                    .card {
+                        background: white;
+                        padding: 60px;
+                        border-radius: 4px;
+                        box-shadow: 0 2px 3px #C8D0D8;
+                        display: inline-block;
+                        margin: 0 auto;
+                    }
+                </style>
+                <body>
+                    <div class="card">
+                        <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
+                            <i class="checkmark">${status === "requires_payment_method" ? "❌" : "✓"}</i>
+                        </div>
+                        <h1>${message}</h1> 
+                        <a href="${href}">${status === "requires_payment_method" ? "Pay again" : "Home"}</a>
+                    </div>
+                </body>
+            </html>
+            `);
+        });
+
+        this.router.post("/webhook", async (req, res) => {
+            const payload = req.body;
+            console.log(Stripe_Webhook_Secret)
+            const sig = req.headers['stripe-signature'] as string;
+
+            //The event
+            let event;
         
+            try {
+                // @ts-ignore
+                event = Stripe.webhooks.constructEvent(req.rawBody, sig, Stripe_Webhook_Secret);
+            } catch (err) {
+                //@ts-ignore
+                return res.status(400).send(`Webhook Error: ${err.message}`);;
+            }
+
+            switch (event.type)
+            {
+                case 'payment_intent.succeeded': {
+                    let payment_intent = event.data.object as any;
+                    const intent = await RetrivePaymentIntent(payment_intent.id);
+                    markInvoicePaid(intent);
+                    break;
+                }
+
+            }
+            return res.sendStatus(200);
+        });
 
     }
 
