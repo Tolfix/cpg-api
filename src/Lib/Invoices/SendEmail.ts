4d5a1f82fe17566012c251d67403ab91c28abe68
Matteus <tolfx@tolfix.com>
2021-11-23 22:04:52 +0100

    Fixed name

--- src/Lib/Invoices/SendEmail.ts
+++ src/Lib/Invoices/SendEmail.ts
@@ -1,5 +1,5 @@
 import { Document } from "mongoose";
-import { Full_Domain } from "../../Config";
+import { Company_Name, Full_Domain } from "../../Config";
 import Footer from "../../Email/Templates/General/Footer";
 import { ICustomer } from "../../Interfaces/Customer";
 import { IInvoice } from "../../Interfaces/Invoice";
@@ -14,7 +14,67 @@ export async function sendInvoiceEmail(invoice: IInvoice & Document, Customer: I
             return;
         
         //@ts-ignore
-        SendEmail(Customer.personal.email, `Invoice from Tolfix ${invoice.id}`, {
+        SendEmail(Customer.personal.email, `Invoice from ${Company_Name ?? "CPG"} ${invoice.id}`, {
+            isHTML: true,
+            attachments: [
+                {
+                    filename: 'invoice.pdf',
+                    content: Buffer.from(await createPDFInvoice(invoice) ?? "==", 'base64'),
+                    contentType: 'application/pdf'
+                }
+            ],
+            body: `Dear ${Customer.personal.first_name} ${Customer.personal.last_name} ${Customer.billing.company ? `(${Customer.billing.company})` : ''} <br />
+            This is a notice that an invoice has been generated on ${invoice.dates.invoice_date}
+            <br />
+            <br />
+            Your payment method is: ${invoice.payment_method}
+            <br />
+            <br />
+            Invoice id: #<strong>${invoice.id}</strong>. <br />
+            Tax due: ${invoice.tax_rate} <br />
+            Amount due: ${invoice.amount+invoice.amount*invoice.tax_rate/100} <br />
+            Due date: ${invoice.dates.due_date} <br />
+
+            ${invoice.payment_method === "paypal" ? `<br />
+            <a href="${Full_Domain}/v2/paypal/pay/${invoice.uid}" target="_blank">
+                Click me to pay.
+            </a>
+            ` : ''}
+            ${invoice.payment_method === "credit_card" ? `<br />
+            <a href="${Full_Domain}/v2/stripe/pay/${invoice.uid}" target="_blank">
+                Click me to pay.
+            </a>
+            ` : ''}
+            <strong>Invoice items</strong> <br />
+            ${invoice.items.map(item => `<br />
+            ${item.notes} <br />
+            ${item.quantity} x ${item.amount} = ${item.quantity * item.amount} <br />
+            `).join('')}
+            <br />
+            ${Footer}
+            `
+        }, (err: any, sent: any) => {
+            if(!err && sent)
+            {
+                invoice.notified = true;
+                invoice.status = "payment_pending";
+                invoice.save();
+            }
+        });
+
+        resolve(true);
+    });
+}
+
+export async function sendLateInvoiceEmail(invoice: IInvoice & Document, Customer: ICustomer)
+{
+    return new Promise(async(resolve, reject) => {
+
+        if(!Customer.personal.email)
+            return;
+        
+        //@ts-ignore
+        SendEmail(Customer.personal.email, `Invoice from ${Company_Name ?? "CPG"} ${invoice.id}`, {
             isHTML: true,
             attachments: [
                 {
