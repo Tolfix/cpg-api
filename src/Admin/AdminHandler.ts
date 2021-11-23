9023446df7a56ddd7e3ed6f8d5bc374198be2347
Matteus <tolfx@tolfix.com>
2021-11-23 19:49:11 +0100

    Added more admin commands

--- src/Admin/AdminHandler.ts
+++ src/Admin/AdminHandler.ts
@@ -1,5 +1,7 @@
+import { stripIndent } from "common-tags";
 import prompt from "prompt";
 import { CacheAdmin } from "../Cache/CacheAdmin";
+import AdminModel from "../Database/Schemas/Administrators";
 import ConfigModel from "../Database/Schemas/Configs";
 import Logger from "../Lib/Logger";
 import createAdmin from "./CreateAdmin";
@@ -18,9 +20,18 @@ export default class AdminHandler
         prompt.get(['action'], async (err, result) => {
 
             if(result.action === "help")
-                Logger.info("Available actions: create, delete, show_admins, help, update_smtp");
+                Logger.info(stripIndent`
+                Available actions:
+                    add_email,
+                    delete_email,
+                    create_admin,
+                    delete_admin,
+                    show_admins,
+                    help,
+                    update_smtp
+                `);
 
-            if(result.action === "create")
+            if(result.action === "create_admin")
                 await this.create();
 
             if(result.action === "show_admins")
@@ -29,6 +40,15 @@ export default class AdminHandler
             if(result.action === "update_smtp")
                 await this.update_smtp();
 
+            if(result.action === "delete_admin")
+                await this.delete_admin();
+
+            if(result.action === "add_email")
+                await this.add_email();
+
+            if(result.action === "delete_email")
+                await this.delete_email();
+            
             this.action();
         });
     }
@@ -114,6 +134,85 @@ export default class AdminHandler
                 Logger.info(`Creating administrator..`);
                 createAdmin(username, password);
                 resolve(true)
+            });
+        });
+    }
+
+    private async delete_admin()
+    {
+        return new Promise((resolve, reject) => {
+            prompt.get([
+                {
+                    name: "username",
+                    description: "Username for administrator",
+                    required: true
+                },
+            ], async (err, result) => {
+                const username = result.username as string;
+                Logger.info(`Deleting administrator..`);
+
+                AdminModel.findOneAndDelete({ username: username }, (err: any) => {
+                    if(err)
+                        Logger.error(err);
+                        
+                    Logger.info(`Successfully deleted administrator ${username}`);
+                });
+                resolve(true)
+            });
+        });
+    }
+
+    private async add_email()
+    {
+        return new Promise((resolve, reject) => {
+            prompt.get([
+                {
+                    name: "email",
+                    description: "Email for administrator",
+                    required: true
+                },
+            ], async (err, result) => {
+                const email = result.email as string;
+                Logger.info(`Adding email..`);
+                // Check if email is valid
+                if(!email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
+                {
+                    Logger.error(`Invalid email`);
+                    return resolve(false);
+                }
+                // Get our config from database
+                const config = (await ConfigModel.find())[0];
+
+                config.smtp_emails.push(email);
+
+                // Save our config
+                await config.save();
+                return resolve(true)
+            });
+        });
+    }
+
+    private async delete_email()
+    {
+        return new Promise((resolve, reject) => {
+            prompt.get([
+                {
+                    name: "email",
+                    description: "Email for administrator",
+                    required: true
+                },
+            ], async (err, result) => {
+                const email = result.email as string;
+                Logger.info(`Deleting email..`);
+                // Get our config from database
+                const config = (await ConfigModel.find())[0];
+
+                // Remove email
+                config.smtp_emails = config.smtp_emails.filter(e => e !== email);
+
+                // Save our config
+                await config.save();
+                return resolve(true)
             });
         });
     }
