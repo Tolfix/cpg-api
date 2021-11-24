f0368fe24590aa813c77988e36dcba73110c42f8
Matteus <tolfx@tolfix.com>
2021-11-24 20:39:03 +0100

    More events for emails

--- src/Events/SendEmailsEvents.ts
+++ src/Events/SendEmailsEvents.ts
@@ -28,3 +28,31 @@ mainEvent.on("invoice_paid", async (invoice) => {
     
     });
 });
+
+// Send email to our admins when invoice is deleted
+mainEvent.on("invoice_deleted", async (invoice) => {
+    // Send email to our admins from config
+    const emails = await GetSMTPEmails();
+    emails.forEach(async (email) => {
+    
+        await SendEmail(email, "Invoice deleted", {
+            isHTML: false,
+            body: `Invoice ${invoice.id} has been deleted`,
+        });
+    
+    });
+});
+
+// Send email to our admins when invoice is notified
+mainEvent.on("invoice_notified", async (invoice) => {
+    // Send email to our admins from config
+    const emails = await GetSMTPEmails();
+    emails.forEach(async (email) => {
+    
+        await SendEmail(email, "Invoice notified", {
+            isHTML: false,
+            body: `Invoice ${invoice.id} has been notified`,
+        });
+    
+    });
+});
