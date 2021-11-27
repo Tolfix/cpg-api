14abb5fe32a0f5dd191270c369fba61f1b1328bd
Matteus <tolfx@tolfix.com>
2021-11-27 15:25:38 +0100

    Fixed EnsureAuth

--- src/Middlewares/EnsureAuth.ts
+++ src/Middlewares/EnsureAuth.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express"
 import jwt from "jsonwebtoken";
 import { JWT_Access_Token } from "../Config";
+import Logger from "../Lib/Logger";
 import { APIError } from "../Lib/Response";
 
 export default function EnsureAuth()
@@ -8,22 +9,44 @@ export default function EnsureAuth()
     return (req: Request, res: Response, next: NextFunction) =>
     {
         const authHeader = req.headers['authorization'];
-        const token = authHeader;
-        if (token == null)
-            return APIError(`Missing token in headers.`, 401)(res);
+        if(!authHeader)
+            return APIError({
+                text: "Missing 'authorization' in header"
+            })(res);
     
-        jwt.verify(token, JWT_Access_Token, (err, payload) =>
+        const b64auth = (authHeader).split(' ');
+    
+        if(!b64auth[0].toLocaleLowerCase().match(/basic|bearer/g))
+            return APIError({
+                text: "Missing 'basic' or 'bearer' in authorization"
+            })(res);
+    
+        if(!b64auth[1])
+            return APIError({
+                text: "Missing 'buffer' in authorization"
+            })(res);
+
+        if(b64auth[0].toLocaleLowerCase() === "bearer")
         {
-            if (err) 
-                return APIError(`Unauthorized user.`, 403)(res);
+            const token = (Buffer.isBuffer(b64auth[1]) ? Buffer.from(b64auth[1], 'base64') : b64auth[1]).toString();
+            jwt.verify(token, JWT_Access_Token, (err, payload) =>
+            {
+                console.log(err);
+                if (err) 
+                    return APIError(`Unauthorized user.`, 403)(res);
+    
+                if(!payload?.data?.id)
+                    return APIError(`Wrong payload.`, 403)(res);
+    
+                //@ts-ignore
+                req.customer = payload.data;
+    
+                Logger.api(`Authorizing`, payload.data);
+    
+                return next();
+            });
+        }
+        
 
-            if(!payload?.data?.id)
-                return APIError(`Wrong payload.`, 403)(res);
-
-            //@ts-ignore
-            req.customer = payload.data;
-
-            return next();
-        });
     }
 }
