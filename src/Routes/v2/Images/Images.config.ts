28e19795769814b52983ab252e42a495af458e0f
Matteus <tolfx@tolfix.com>
2021-11-28 19:07:59 +0100

    Added images

--- src/Routes/v2/Images/Images.config.ts
+++ src/Routes/v2/Images/Images.config.ts
@@ -1,1 +1,45 @@
-export {};
+import { Application, Router } from "express";
+import EnsureAdmin from "../../../Middlewares/EnsureAdmin";
+import ImageController from "./Images.controller";
+
+export default class ProductsRouter
+{
+    private server: Application;
+    private router = Router();
+
+    constructor(server: Application, version: string)
+    {
+        this.server = server;
+        this.server.use(`/${version}/images`, this.router);
+
+        this.router.get("/", [
+            ImageController.list
+        ]);
+
+        this.router.get("/:uid", [
+            ImageController.getByUid
+        ]);
+
+        this.router.post("/", [
+            EnsureAdmin,
+            ImageController.insert
+        ]);
+
+        this.router.patch("/:uid", [
+            EnsureAdmin,
+            ImageController.patch
+        ]);
+
+        this.router.put("/:uid", [
+            EnsureAdmin,
+            ImageController.patch
+        ]);
+
+        this.router.delete("/:uid", [
+            EnsureAdmin,
+            ImageController.removeById
+        ]);
+
+    }
+
+}
