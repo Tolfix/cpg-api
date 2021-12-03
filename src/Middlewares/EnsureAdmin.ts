9374217b9bb7b4f803195e293e8d7f28789dc04a
Matteus <tolfx@tolfix.com>
2021-12-03 19:19:47 +0100

    Modified < EnsureAdmin >

--- src/Middlewares/EnsureAdmin.ts
+++ src/Middlewares/EnsureAdmin.ts
@@ -10,21 +10,15 @@ export default function EnsureAdmin(req: Request, res: Response, next: NextFunct
 {
     const authHeader = req.headers['authorization'];
     if(!authHeader)
-        return APIError({
-            text: "Missing 'authorization' in header"
-        })(res);
+        return APIError("Missing 'authorization' in header")(res);
 
     const b64auth = (authHeader).split(' ');
 
     if(!b64auth[0].toLocaleLowerCase().match(/basic|bearer/g))
-        return APIError({
-            text: "Missing 'basic' or 'bearer' in authorization"
-        })(res);
+        return APIError("Missing 'basic' or 'bearer' in authorization")(res);
 
     if(!b64auth[1])
-        return APIError({
-            text: "Missing 'buffer' in authorization"
-        })(res);
+        return APIError("Missing 'buffer' in authorization")(res);
 
     if(b64auth[0].toLocaleLowerCase() === "basic")
     {
@@ -32,9 +26,7 @@ export default function EnsureAdmin(req: Request, res: Response, next: NextFunct
 
         return bcrypt.compare(password, (CacheAdmin.get(getAdminByUsername(login) ?? "ADM_")?.["password"]) ?? "", (err, match) => {
             if(!match)
-                return APIError({
-                    text: "Unauthorized admin"
-                }, 403)(res);
+                return APIError("Unauthorized admin", 403)(res);
     
             return next();
         });
@@ -45,10 +37,8 @@ export default function EnsureAdmin(req: Request, res: Response, next: NextFunct
         const token = (Buffer.isBuffer(b64auth[1]) ? Buffer.from(b64auth[1], 'base64') : b64auth[1]).toString();
         jwt.verify(token, JWT_Access_Token, (err, payload) => {
             if(err || !payload)
-                return APIError({
-                    text: "Unauthorized"
-                }, 403)(res);
-            Logger.debug(payload)
+                return APIError("Unauthorized", 403)(res);
+
             return next();
         });
     }
