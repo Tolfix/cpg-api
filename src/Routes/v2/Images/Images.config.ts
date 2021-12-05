b922eef93cf15006bae24e245881ca56a18a428e
Matteus <tolfx@tolfix.com>
2021-12-05 21:45:51 +0100

    Fixed `Images.config.ts`

--- src/Routes/v2/Images/Images.config.ts
+++ src/Routes/v2/Images/Images.config.ts
@@ -1,8 +1,15 @@
 import { Application, Router } from "express";
+import Logger from "../../../Lib/Logger";
+import { APIError, APISuccess } from "../../../Lib/Response";
 import EnsureAdmin from "../../../Middlewares/EnsureAdmin";
-import ImageController from "./Images.controller";
+import { CacheImages } from "../../../Cache/CacheImage";
+import { IImage } from "../../../Interfaces/Images";
+import { UploadedFile } from "express-fileupload";
+import { idImages } from "../../../Lib/Generator";
+import ImageModel from "../../../Database/Schemas/Images";
+import AW from "../../../Lib/AW";
 
-export default class ProductsRouter
+export default class ImagesRouter
 {
     private server: Application;
     private router = Router();
@@ -12,34 +19,104 @@ export default class ProductsRouter
         this.server = server;
         this.server.use(`/${version}/images`, this.router);
 
-        this.router.get("/", [
-            ImageController.list
-        ]);
+        /**
+         * Gets all images except buffer.
+         * @route GET /images
+         * @group Images
+         * @returns {Images} 200 - Images data
+         * @security JWT
+         */
+        this.router.get("/", EnsureAdmin, (req, res) => {
+            return APISuccess(CacheImages.array().map(e => {
+                return {
+                    uid: e.uid,
+                    name: e.name,
+                    type: e.type,
+                    size: e.size
+                }
+            }))(res);
+        });
+        /**
+         * Gets image with buffer
+         * @route GET /images/{uid}
+         * @group Images
+         * @param {string} uid.path.required - uid for image
+         * @returns {Images} 200 - Image data in base64
+         * @returns {APIError} 404 - Failed to find
+         * @security Basic
+         */
+        this.router.get("/:id", EnsureAdmin, (req, res) => {
+            const id = req.params.id as IImage["id"];
 
-        this.router.get("/:uid", [
-            ImageController.getByUid
-        ]);
+            const data = CacheImages.get(id);
 
-        this.router.post("/", [
-            EnsureAdmin,
-            ImageController.insert
-        ]);
+            if(!data)
+                return APIError(`Unable to find image by id ${id}`)(res);
 
-        this.router.patch("/:uid", [
-            EnsureAdmin,
-            ImageController.patch
-        ]);
+            let binstr = Array.prototype.map.call(data.data, function (ch) {
+                return String.fromCharCode(ch);
+            }).join('');
+            
+            let d = btoa(binstr);
 
-        this.router.put("/:uid", [
-            EnsureAdmin,
-            ImageController.patch
-        ]);
+            return APISuccess(d)(res);
+        });
 
-        this.router.delete("/:uid", [
-            EnsureAdmin,
-            ImageController.removeById
-        ]);
+        /**
+         * Gets image with buffer
+         * @route POST /images
+         * @group Images
+         * @param {file} image.formData.required Image data as formdata
+         * @security Basic
+         */
+        this.router.post("/", EnsureAdmin, async (req, res) => {
+            if(req.files)
+            {
+                const image = (req.files.image as UploadedFile);
 
+                Logger.debug(`Uploading image ${image.name}`);
+
+                let dataImage = {
+                    uid: idImages(),
+                    data: image.data,
+                    type: image.mimetype,
+                    size: image.size,
+                    name: image.name
+                };
+
+                const db_Image = await new ImageModel(dataImage).save();
+                
+                // @ts-ignore
+                CacheImages.set(db_Image.id, db_Image);
+
+                return APISuccess(db_Image.id)(res);
+            }
+            return APIError({
+                text: `Failed to create image`,
+            })(res);
+        });
+
+        /**
+         * Deletes image
+         * @route DELETE /images/{uid}
+         * @group Images
+         * @param {string} uid.path.required - uid for image
+         * @security Basic
+         */
+         this.router.delete("/:id", EnsureAdmin, async (req, res) => {
+            const id = req.params.id as IImage["id"];
+            const data = CacheImages.get(id);
+            if(!data)
+                return APIError(`Unable to find image by id ${id}`)(res);
+            
+            const [S, F] = await AW(ImageModel.deleteOne({ id: id }));
+            
+            if(F)
+                return APIError(`Something went wrong.. try again later`)(res);
+            
+            CacheImages.delete(id);
+            
+            APISuccess(`Succesfully delete image`)(res);
+        });
     }
-
-}
+} 
