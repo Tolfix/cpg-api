928abe4f4e785bb8ee9ccb4dd5228351ff038983
Matteus <tolfx@tolfix.com>
2021-12-20 20:09:09 +0100

    New < Quotes routes >

---
+++ src/Routes/v2/Quotes/Quotes.controller.ts
@@ -0,0 +1,78 @@
+import { Request, Response } from "express";
+import QuotesModel from "../../../Database/Schemas/Quotes";
+import mainEvent from "../../../Events/Main";
+import { IQuotes } from "../../../Interfaces/Quotes";
+import { idTransicitons } from "../../../Lib/Generator";
+import { APISuccess } from "../../../Lib/Response";
+import BaseModelAPI from "../../../Models/BaseModelAPI";
+
+const API = new BaseModelAPI<IQuotes & { uid: any }>(idTransicitons, QuotesModel);
+
+function insert(req: Request, res: Response)
+{
+    API.create(req.body)
+        .then((result) => {
+            
+            mainEvent.emit("quotes_created", result);
+
+            APISuccess({
+                uid: result.uid
+            })(res);
+        });
+}
+
+function getByUid(req: Request, res: Response)
+{
+    API.findByUid((req.params.uid)).then((result) => {
+        APISuccess(result)(res);
+    });
+}
+
+function list(req: Request, res: Response)
+{
+    let limit = parseInt(req.query._end as string)
+    && parseInt(req.query._end as string) <= 100 ? 
+                                                parseInt(req.query._end as string) 
+                                                :
+                                                25;
+    let start = 0;
+    if(req.query)
+        if(req.query._start)
+            start = Number.isInteger(parseInt(req.query._start as string)) ? parseInt(req.query._start as string) : 0;
+    
+    let sort = req.query._sort as string ?? "id";
+    let order = req.query._order as string ?? "asc";
+        
+    API.findAll(limit, start, sort, order).then((result: any) => {
+        APISuccess(result)(res)
+    });
+}
+
+function patch(req: Request, res: Response)
+{
+    API.findAndPatch((req.params.uid), req.body).then((result) => {
+        // @ts-ignore
+        mainEvent.emit("quotes_updated", result);
+        APISuccess(result)(res);
+    });
+}
+
+function removeById(req: Request, res: Response)
+{
+    API.removeByUid(req.params.uid)
+        .then((result)=>{
+            // @ts-ignore
+            mainEvent.emit("quotes_deleted", result);
+            APISuccess(result, 204)(res)
+        });
+ };
+
+const QuotesController = {
+    insert,
+    getByUid,
+    list,
+    patch,
+    removeById
+}
+
+export default QuotesController;
