c460e2ac0b3d73edf1dfbab7b9487abce8ac8a6e
Tolfx <matteus@tolfx.dev>
2022-01-05 00:59:02 +0100

    New types

---
+++ src/Types/PaymentMethod.ts
@@ -0,0 +1,21 @@
+import { IPayments } from "../Interfaces/Payments";
+
+export type TPayments = "none" | "manual" | "bank" | "paypal" | "credit_card" | "swish";
+export const A_CC_Payments = [
+    "credit_card",
+    "paypal",
+    "swish",
+    "none",
+    "manual",
+    "bank"
+]
+
+export type TRecurringMethod = "monthly" | "yearly" | "quarterly" | "semi_annually" | "biennially" | "triennially";
+export const A_RecurringMethod = [
+    "monthly",
+    "yearly",
+    "quarterly",
+    "semi_annually",
+    "biennially",
+    "triennially"
+]
