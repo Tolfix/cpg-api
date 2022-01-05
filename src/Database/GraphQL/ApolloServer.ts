7f395f1c24e9b7ad8d28903ff749db5439537ae4
Tolfx <matteus@tolfx.dev>
2022-01-05 23:57:19 +0100

    Modified < ApolloServer >

--- src/Database/GraphQL/ApolloServer.ts
+++ src/Database/GraphQL/ApolloServer.ts
@@ -40,15 +40,17 @@ export default async (server: any) =>
                     password = login.split(":")[1];
                     login = login.split(":")[0];
                 }
-                
+
                 // Check if admin
                 if(CacheAdmin.has(getAdminByUsername(login) ?? "ADM_"))
                 {
                     let succeed = await bcrypt.compare(password, (CacheAdmin.get(getAdminByUsername(login) ?? "ADM_")?.["password"] ?? ""));
 
-                    if(!succeed)
+                    if(succeed)
                         return {
-                            isAuth: false,
+                            isAuth: true,
+                            isAdmin: true,
+                            isUser: false,
                         }
                 }
             }
@@ -56,21 +58,28 @@ export default async (server: any) =>
             if(b64auth[0].toLocaleLowerCase() === "bearer")
             {
                 const token = (Buffer.isBuffer(b64auth[1]) ? Buffer.from(b64auth[1], 'base64') : b64auth[1]).toString();
-                jwt.verify(token, JWT_Access_Token, (err, payload) => {
-                    if(err || !payload)
-                        return {
-                            isAuth: false,
-                        }
-
-
+                let suc = jwt.verify(token, JWT_Access_Token)
+                // @ts-ignore
+                if(suc?.data === "admin")
+                return {
+                    isAuth: true,
+                    isAdmin: true,
+                    isUser: false,
+                }
+                
+                // @ts-ignore
+                if(suc?.data?.["id"])
                     return {
                         isAuth: true,
+                        isAdmin: false,
+                        isUser: true,
                     }
-                });
             }
 
             return {
                 isAuth: false,
+                isAdmin: false,
+                isUser: false,
             }
         },
     });
