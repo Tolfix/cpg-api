83b624a808729097038dd6bf69d33bc6c380f6bf
Tolfx <matteus@tolfx.dev>
2022-01-05 21:52:56 +0100

    Testing "adminaccess"

--- src/Database/GraphQL/Schemas/Categories.ts
+++ src/Database/GraphQL/Schemas/Categories.ts
@@ -1,10 +1,28 @@
 import { composeWithMongoose } from "graphql-compose-mongoose";
 import CategoryModel from "../../Models/Category";
 
+function adminAccess(resolvers) {
+    Object.keys(resolvers).forEach((k) => {
+      resolvers[k] = resolvers[k].wrapResolve(next => async rp => {
+  
+        // extend resolve params with hook
+        rp.beforeRecordMutate = async function(doc, rp) { 
+                if (!rp.context.isAuth) {
+                    throw new Error("Not Authorized");
+                }
+                return doc;
+            }
+  
+        return next(rp)
+      })
+    })
+    return resolvers
+}
+
 const CategoriesGraphQL = composeWithMongoose(CategoryModel);
 export const startsWith = "Categories";
 export const CategoriesQuery = {
-    categoryById: CategoriesGraphQL.getResolver("findById"),
+    categoryById: CategoriesGraphQL.mongooseResolver.findById(),
     categoryByIds: CategoriesGraphQL.getResolver("findByIds"),
     categoryOne: CategoriesGraphQL.getResolver("findOne"),
     categoryMany: CategoriesGraphQL.getResolver("findMany"),
@@ -14,12 +32,14 @@ export const CategoriesQuery = {
 }
 
 export const CategoriesMutation = {
-    categoryCreateOne: CategoriesGraphQL.getResolver("createOne"),
-    categoryCreateMany: CategoriesGraphQL.getResolver("createMany"),
-    categoryUpdateById: CategoriesGraphQL.getResolver("updateById"),
-    categoryUpdateOne: CategoriesGraphQL.getResolver("updateOne"),
-    categoryUpdateMany: CategoriesGraphQL.getResolver("updateMany"),
-    categoryRemoveById: CategoriesGraphQL.getResolver("removeById"),
-    categoryRemoveOne: CategoriesGraphQL.getResolver("removeOne"),
-    categoryRemoveMany: CategoriesGraphQL.getResolver("removeMany"),
+    adminAccess({
+        categoryCreateOne: CategoriesGraphQL.getResolver("createOne"),
+        categoryCreateMany: CategoriesGraphQL.getResolver("createMany"),
+        categoryUpdateById: CategoriesGraphQL.getResolver("updateById"),
+        categoryUpdateOne: CategoriesGraphQL.getResolver("updateOne"),
+        categoryUpdateMany: CategoriesGraphQL.getResolver("updateMany"),
+        categoryRemoveById: CategoriesGraphQL.getResolver("removeById"),
+        categoryRemoveOne: CategoriesGraphQL.getResolver("removeOne"),
+        categoryRemoveMany: CategoriesGraphQL.getResolver("removeMany"),
+    })
 };
