ecb26aad70ef3491c2d0473191b15d171e93cb9d
Tolfx <matteus@tolfx.dev>
2022-01-08 23:11:44 +0100

    Lint fix `getProductById.ts`

--- src/Lib/Products/getProductById.ts
+++ src/Lib/Products/getProductById.ts
@@ -1,6 +1,7 @@
 import ProductModel from "../../Database/Models/Products";
 
-export default async (id: number) => {
+export default async (id: number) =>
+{
     const product = await ProductModel.findOne({ id: id });
     if(!product)
         return null;
