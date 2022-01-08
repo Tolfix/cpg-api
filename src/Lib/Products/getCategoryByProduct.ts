47997e6be2ac8a815dbed67541e6788d5249b159
Tolfx <matteus@tolfx.dev>
2022-01-08 23:11:34 +0100

    Lint fix `getCategoryByproduct.ts`

--- src/Lib/Products/getCategoryByProduct.ts
+++ src/Lib/Products/getCategoryByProduct.ts
@@ -1,6 +1,7 @@
 import CategoryModel from "../../Database/Models/Category";
 import { IProduct } from "../../Interfaces/Products";
 
-export default async (product: IProduct) => {
+export default async (product: IProduct) =>
+{
     return await CategoryModel.findOne({ id: product.category_uid });
 }
