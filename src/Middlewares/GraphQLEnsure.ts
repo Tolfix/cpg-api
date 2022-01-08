77d97e0340d820117e8bd9aedb5646d23f7f85ab
Tolfx <matteus@tolfx.dev>
2022-01-08 23:12:10 +0100

    Lint fix `GraphQLEnsure.ts`

--- src/Middlewares/GraphQLEnsure.ts
+++ src/Middlewares/GraphQLEnsure.ts
@@ -1,5 +1,6 @@
 import { skip } from 'graphql-resolvers';
-export default (parent: any, args: any, { isAuth }: any) => {
+export default (parent: any, args: any, { isAuth }: any) =>
+{
     if(!isAuth)
         throw new Error("Not authenticated");
 
