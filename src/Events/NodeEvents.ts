8575749f309d673cbc9bfc998302a71792f3b8e3
Tolfx <matteus@tolfx.dev>
2022-01-08 23:10:01 +0100

    Lint fix `NodeEvents.ts`

--- src/Events/NodeEvents.ts
+++ src/Events/NodeEvents.ts
@@ -1,13 +1,16 @@
 import Logger from "../Lib/Logger";
 
-process.on('exit', (code) => {
+process.on('exit', (code) =>
+{
     Logger.error(`About to exit with code`, code);
 });
 
-process.on('unhandledRejection', (reason, promise) => {
+process.on('unhandledRejection', (reason, promise) =>
+{
     Logger.error('Unhandled Rejection at: ', promise, ' reason: ', reason);
 });
 
-process.on('uncaughtExceptionMonitor', (err: any, origin: any) => {
+process.on('uncaughtExceptionMonitor', (err: any, origin: any) =>
+{
     Logger.error(`An error`, err, "at ", origin);
 });
