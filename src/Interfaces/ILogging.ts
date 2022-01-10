9727f171b4de2b60474669729dcbc38cb6c95849
Tolfx <matteus@tolfx.dev>
2022-01-10 16:03:04 +0100

    New < ILogging.ts >

---
+++ src/Interfaces/ILogging.ts
@@ -0,0 +1,14 @@
+export interface ILoggingTypes
+{
+    debug: <T extends any[]>(...body: T) => void;
+    api: <T extends any[]>(...body: T) => void;
+    graphql: <T extends any[]>(...body: T) => void;
+    plugin: <T extends any[]>(...body: T) => void;
+    cache: <T extends any[]>(...body: T) => void;
+    rainbow: <T extends any[]>(...body: T) => void;
+    verbos: <T extends any[]>(...body: T) => void;
+    error: <T extends any[]>(...body: T) => void;
+    warning: <T extends any[]>(...body: T) => void;
+    info: <T extends any[]>(...body: T) => void;
+    db: <T extends any[]>(...body: T) => void;
+}
