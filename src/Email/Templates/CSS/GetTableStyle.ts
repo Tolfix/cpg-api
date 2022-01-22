83e229b2a43a92f33397410f2a975e18cecc6e5e
Tolfx <matteus@tolfx.dev>
2022-01-22 14:03:28 +0100

    Styles for table

---
+++ src/Email/Templates/CSS/GetTableStyle.ts
@@ -0,0 +1,19 @@
+export default `
+width: 100%;
+border-collapse: collapse;
+    thead th,
+    tfoot th,
+    thead th,
+    tfoot th {
+        border-bottom: 1px solid #999;
+        text-align: left;
+        margin: 0;
+        padding: 7px 5px;
+    }
+    tbody tr td,
+    tbody tr td {
+        margin: 0;
+        padding: 7px 5px;
+        border-top: 1px solid #E6E6E6;
+    }
+`
