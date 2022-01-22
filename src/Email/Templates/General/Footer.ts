f71ec800fd81785b39efeedbd855245f8607faac
Tolfx <matteus@tolfx.dev>
2022-01-22 16:29:20 +0100

    Modified Footer/Header

--- src/Email/Templates/General/Footer.ts
+++ src/Email/Templates/General/Footer.ts
@@ -1,8 +1,10 @@
 export default `
-<div style="font-size: 21px;
-color: #999;
-padding: 20px 5px 10px;
-text-align: center;">
-    <a href="https://github.com/Tolfix/cpg-api">cpg-api</a> &copy; 
+<div style="
+    font-size: 21px;
+    color: #999;
+    padding: 20px 5px 10px;
+    text-align: center;
+">
+    &copy; <a href="https://github.com/Tolfix/cpg-api">CPG-API</a>
 </div>
 `;
--- src/Email/Templates/General/Header.ts
+++ src/Email/Templates/General/Header.ts
@@ -1,11 +1,13 @@
-import { Company_Logo_Url, Company_Name } from "../../../Config";
+import { Company_Logo_Url, Company_Name, Company_Website } from "../../../Config";
 
 export default async () => `
 <div style="margin-bottom: 5px;
 padding: 10px 0;
 text-align: center;">
     <div class="logo">
-        <img width="256" src="${await Company_Logo_Url()}" alt="${await Company_Name()}" />
+        <a href="${await Company_Website()}" target="_blank">
+            <img width="256" src="${await Company_Logo_Url()}" alt="${await Company_Name()}" />
+        </a>
     </div>
 </div>
 `;
