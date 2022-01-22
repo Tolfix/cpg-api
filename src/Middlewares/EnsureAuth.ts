cc6c02bf0ce55f60f4f0f6e61e9ae15d7a0a8dc0
Tolfx <matteus@tolfx.dev>
2022-01-22 14:07:48 +0100

    Fixed typing

--- src/Middlewares/EnsureAuth.ts
+++ src/Middlewares/EnsureAuth.ts
@@ -35,9 +35,8 @@ export default function EnsureAuth()
                     return APIError(`Unauthorized user.`, 403)(res);
 
                 // @ts-ignore
-                if(payload?.data?.id !== 0)
-                    if(!payload?.data?.id)
-                        return APIError(`Wrong payload.`, 403)(res);
+                if(!payload?.data?.id)
+                    return APIError(`Wrong payload.`, 403)(res);
     
                 //@ts-ignore
                 req.customer = payload.data;
