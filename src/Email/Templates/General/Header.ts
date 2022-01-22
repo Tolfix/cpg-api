6e5f9356e86ab7003f6c1bc7f84f05e0e315603c
Tolfx <matteus@tolfx.dev>
2022-01-22 14:03:54 +0100

    Stylish email header/footer #52

--- src/Email/Templates/General/Header.ts
+++ src/Email/Templates/General/Header.ts
@@ -1,3 +1,11 @@
-export default `
+import { Company_Logo_Url, Company_Name } from "../../../Config";
 
+export default async () => `
+<div style="margin-bottom: 5px;
+padding: 10px 0;
+text-align: center;">
+    <div class="logo">
+        <img width="256" src="${await Company_Logo_Url()}" alt="${await Company_Name()}" />
+    </div>
+</div>
 `;
