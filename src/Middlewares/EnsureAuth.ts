6788c94d59af153d193eac407aec3b9903f0b23b
Tolfx <matteus@tolfx.dev>
2022-01-28 17:38:39 +0100

    Accepts query authentication

--- src/Middlewares/EnsureAuth.ts
+++ src/Middlewares/EnsureAuth.ts
@@ -9,25 +9,35 @@ export default function EnsureAuth()
     return (req: Request, res: Response, next: NextFunction) =>
     {
         const authHeader = req.headers['authorization'];
-        if(!authHeader)
+        const tokenQuery = req.query.access_token;
+        if(!authHeader && !tokenQuery)
             return APIError({
                 text: "Missing 'authorization' in header"
             })(res);
     
-        const b64auth = (authHeader).split(' ');
+        let b64auth: string[];
+        if(authHeader)
+            b64auth = authHeader.split(' ');
     
-        if(!b64auth[0].toLocaleLowerCase().match(/basic|bearer/g))
+        if(tokenQuery)
+            b64auth = ["query", tokenQuery as string];
+
+        // @ts-ignore
+        if(!b64auth[0].toLocaleLowerCase().match(/query|bearer/g))
             return APIError({
                 text: "Missing 'basic' or 'bearer' in authorization"
             })(res);
-    
+
+        // @ts-ignore
         if(!b64auth[1])
             return APIError({
                 text: "Missing 'buffer' in authorization"
             })(res);
-
-        if(b64auth[0].toLocaleLowerCase() === "bearer")
+            
+        // @ts-ignore
+        if(b64auth[0].toLocaleLowerCase() === "bearer" || b64auth[0].toLocaleLowerCase() === "query")
         {
+            // @ts-ignore
             const token = (Buffer.isBuffer(b64auth[1]) ? Buffer.from(b64auth[1], 'base64') : b64auth[1]).toString();
             jwt.verify(token, JWT_Access_Token, (err, payload) =>
             {
