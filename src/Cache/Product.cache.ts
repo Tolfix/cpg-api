12ccadbcc380980950feb416c7edf2e9c0e0b358
Tolfx <matteus@tolfx.dev>
2022-02-11 23:55:31 +0100

    Testing

--- src/Cache/Product.cache.ts
+++ src/Cache/Product.cache.ts
@@ -8,5 +8,5 @@ export const CacheProduct = new Map<IProduct["uid"], IProduct>();
 
 export function getProductByCategoryUid(uid: ICategory["uid"])
 {
-    return CacheProduct.array().filter(e => e.category_uid === uid);
+    // return CacheProduct.array().filter(e => e.category_uid === uid);
 }
