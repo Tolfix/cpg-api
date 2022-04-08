3453109715451cbf65019a35f81558a1d7ddd5c9
Tolfx <matteus@tolfx.dev>
2022-04-08 10:59:43 +0200

    feat: :sparkles: Added email prompt

--- src/Admin/AdminHandler.ts
+++ src/Admin/AdminHandler.ts
@@ -1,26 +1,12 @@
-import { stripIndent } from "common-tags";
 import prompt from "prompt";
-import { CacheAdmin } from "../Cache/Admin.cache";
-import { cron_chargeStripePayment, cron_notifyInvoices, cron_notifyLateInvoicePaid } from "../Cron/Methods/Invoices.cron.methods";
-import AdminModel from "../Database/Models/Administrators.model";
 import ConfigModel from "../Database/Models/Configs.model";
 import Logger from "../Lib/Logger";
 import { getPlugins, installPlugin } from "../Plugins/PluginHandler";
-import createAdmin from "./CreateAdmin";
 import chalk from "chalk";
 import clear from "clear";
 import figlet from "figlet";
 import Prompt, { cacheCommands } from "./Commands/Prompt";
 import inquirer from 'inquirer';
-
-export interface ICommandsAdmin
-{
-    [key: string]: {
-        description: string;
-        method: any;
-        [key: string]: any;
-    }
-}
 
 export default class AdminHandler
 {
@@ -67,79 +53,8 @@ export default class AdminHandler
             }
         }).catch((error) =>
         {
-            console.log(error)
-        });
-    }
-
-
-    private async show_emails()
-    {
-        return new Promise(async (resolve,) =>
-        {
-            // Get our config from database
-            const config = (await ConfigModel.find())[0];
-            Logger.info(`Emails:`, config.smtp_emails);
-            resolve(true);
-        });
-    }
-
-    private async add_email()
-    {
-        return new Promise((resolve) =>
-        {
-            prompt.get([
-                {
-                    name: "email",
-                    description: "Email for administrator",
-                    required: true
-                },
-            ], async (err, result) =>
-            {
-                const email = result.email as string;
-                Logger.info(`Adding email..`);
-                // Check if email is valid
-                // eslint-disable-next-line no-useless-escape
-                if(!email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
-                {
-                    Logger.error(`Invalid email`);
-                    return resolve(false);
-                }
-                // Get our config from database
-                const config = (await ConfigModel.find())[0];
-
-                config.smtp_emails.push(email);
-
-                // Save our config
-                await config.save();
-                return resolve(true)
-            });
-        });
-    }
-
-    private async delete_email()
-    {
-        return new Promise((resolve) =>
-        {
-            prompt.get([
-                {
-                    name: "email",
-                    description: "Email for administrator",
-                    required: true
-                },
-            ], async (err, result) =>
-            {
-                const email = result.email as string;
-                Logger.info(`Deleting email..`);
-                // Get our config from database
-                const config = (await ConfigModel.find())[0];
-
-                // Remove email
-                config.smtp_emails = config.smtp_emails.filter(e => e !== email);
-
-                // Save our config
-                await config.save();
-                return resolve(true)
-            });
+            Logger.error(error);
+            this.action();
         });
     }
 
