d5cfcfc70c10e37ac508b1e56733851e81f58610
Tolfx <matteus@tolfx.dev>
2022-04-16 15:26:36 +0200

    feat: :sparkles: Styling reports

--- src/Email/Reports/InvoiceReport.ts
+++ src/Email/Reports/InvoiceReport.ts
@@ -1,25 +1,29 @@
 import { GetSMTPEmails } from "../../Config";
 import { IInvoice } from "@interface/Invoice.interface";
 import { SendEmail } from "../Send";
+import UseStyles from "../Templates/General/UseStyles";
+import { stripIndent } from "common-tags";
 
 export const InvoiceNotifiedReport = async (invoices: IInvoice[]) =>
 {
-    GetSMTPEmails().then((emails) =>
+    GetSMTPEmails().then(async (emails) =>
     {
-        for(const email of emails)
+        for await(const email of emails)
         {
             SendEmail(email, "Invoice(s) Notified", {
                 isHTML: true,
-                body: `
-                <h1>Invoice(s) Notified</h1>
-                <p>
-                    ${invoices.length} invoices have been notified.
-                </p>
-                <p>
-                    The following invoices have been notified:
-                    ${invoices.map((invoice) => `<br><strong>${invoice.id}</strong>`).join("")}
-                </p>
-                `
+                body: await UseStyles(stripIndent`
+                <div>
+                    <h1>Invoice(s) Notified</h1>
+                    <p>
+                        ${invoices.length} invoices have been notified.
+                    </p>
+                    <p>
+                        The following invoices have been notified:
+                        ${invoices.map((invoice) => `<br><strong>${invoice.id}</strong>`).join("")}
+                    </p>
+                </div>
+                `)
             });
         }
     })
@@ -27,22 +31,24 @@ export const InvoiceNotifiedReport = async (invoices: IInvoice[]) =>
 
 export const InvoiceLateReport = async (invoices: IInvoice[]) =>
 {
-    GetSMTPEmails().then((emails) =>
+    GetSMTPEmails().then(async (emails) =>
     {
-        for(const email of emails)
+        for await(const email of emails)
         {
             SendEmail(email, "Invoice(s) Late Reminder", {
                 isHTML: true,
-                body: `
-                <h1>Invoice(s) Reminded</h1>
-                <p>
-                    ${invoices.length} invoices have been reminded.
-                </p>
-                <p>
-                    The following invoices have been reminded:
-                    ${invoices.map((invoice) => `<br><strong>${invoice.id}</strong>`).join("")}
-                </p>
-                `
+                body: await UseStyles(stripIndent`
+                <div>
+                    <h1>Invoice(s) Reminded</h1>
+                    <p>
+                        ${invoices.length} invoices have been reminded.
+                    </p>
+                    <p>
+                        The following invoices have been reminded:
+                        ${invoices.map((invoice) => `<br><strong>${invoice.id}</strong>`).join("")}
+                    </p>
+                </div>
+                `)
             });
         }
     })
@@ -50,22 +56,24 @@ export const InvoiceLateReport = async (invoices: IInvoice[]) =>
 
 export const InvoiceCreatedReport = async (invoices: IInvoice[]) =>
 {
-    GetSMTPEmails().then((emails) =>
+    GetSMTPEmails().then(async (emails) =>
     {
-        for(const email of emails)
+        for await(const email of emails)
         {
             SendEmail(email, "Invoice(s) Created", {
                 isHTML: true,
-                body: `
-                <h1>Invoice(s) Created</h1>
-                <p>
-                    ${invoices.length} invoices have been created.
-                </p>
-                <p>
-                    The following invoices have been created:
-                    ${invoices.map((invoice) => `<br><strong>${invoice.id}</strong>`).join("")}
-                </p>
-                `
+                body: await UseStyles(stripIndent`
+                <div>
+                    <h1>Invoice(s) Created</h1>
+                    <p>
+                        ${invoices.length} invoices have been created.
+                    </p>
+                    <p>
+                        The following invoices have been created:
+                        ${invoices.map((invoice) => `<br><strong>${invoice.id}</strong>`).join("")}
+                    </p>
+                </div>
+                `)
             });
         }
     })
