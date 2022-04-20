e34b091875a3565214c590244e1bbbfb3758eaf3
Tolfx <matteus@tolfx.dev>
2022-04-20 17:52:21 +0200

    feat: :art: Fixed `??` to `||=`

--- src/Config.ts
+++ src/Config.ts
@@ -10,40 +10,40 @@ export const DebugMode = process.env.DEBUG === "true";
 export const HomeDir = ((__dirname.replace("\\build", "")).replace("/build", ""));
 export const JWT_Access_Token = process.env.JWT_ACCESS_TOKEN ?? "";
 export const d_Days = parseInt(process.env.D_DAYS ?? "30");
-export const Domain = process.env.DOMAIN ?? "localhost";
-export const Http_Schema = process.env.HTTP_SCHEMA ?? "http";
-export const PORT = process.env.PORT ?? 8080;
+export const Domain = process.env.DOMAIN ||= "localhost";
+export const Http_Schema = process.env.HTTP_SCHEMA ||= "http";
+export const PORT = process.env.PORT ||= "8080";
 export const Full_Domain = `${Http_Schema}://${Domain === "localhost" ? `localhost:${PORT}` : Domain}`;
 
 // API
 export const Express_Session_Secret = process.env.SESSION_SECRET ?? require("crypto").randomBytes(20).toString("hex");
 
 // Database
-export const MongoDB_URI = process.env.MONGO_URI ?? "mongodb://localhost/cpg";
+export const MongoDB_URI = process.env.MONGO_URI ||= "mongodb://localhost/cpg";
 //  // Postgres
-export const Postgres_User = process.env.POSTGRES_USER ?? "";
-export const Postgres_Password = process.env.POSTGRES_PASSWORD ?? "";
-export const Postgres_Database = process.env.POSTGRES_DATABASE ?? "";
-export const Postgres_Port = parseInt(process.env.POSTGRES_PORT ?? "5432");
-export const Postgres_Host = process.env.POSTGRES_HOST ?? "localhost";
+export const Postgres_User = process.env.POSTGRES_USER ||= "";
+export const Postgres_Password = process.env.POSTGRES_PASSWORD ||= "";
+export const Postgres_Database = process.env.POSTGRES_DATABASE ||= "";
+export const Postgres_Port = parseInt(process.env.POSTGRES_PORT ||= "5432");
+export const Postgres_Host = process.env.POSTGRES_HOST ||= "localhost";
 
 // osTicket configs
-export const osticket_url = process.env.OSTICKET_URL ?? "";
-export const osticket_api_key = process.env.OSTICKET_API_KEY ?? "";
+export const osticket_url = process.env.OSTICKET_URL ||= "";
+export const osticket_api_key = process.env.OSTICKET_API_KEY ||= "";
  
 // Stripe
-export const Stripe_SK_Test = process.env.STRIPE_SK_TEST ?? "";
-export const Stripe_SK_Live = process.env.STRIPE_SK_LIVE ?? "";
-export const Stripe_PK_Public_Test = process.env.STRIPE_SK_PUBLIC_TEST ?? "";
-export const Stripe_PK_Public = process.env.STRIPE_SK_PUBLIC ?? "";
-export const Stripe_Webhook_Secret = process.env.STRIPE_WEBHOOK_SECRET ?? "";
+export const Stripe_SK_Test = process.env.STRIPE_SK_TEST ||= "";
+export const Stripe_SK_Live = process.env.STRIPE_SK_LIVE ||= "";
+export const Stripe_PK_Public_Test = process.env.STRIPE_SK_PUBLIC_TEST ||= "";
+export const Stripe_PK_Public = process.env.STRIPE_SK_PUBLIC ||= "";
+export const Stripe_Webhook_Secret = process.env.STRIPE_WEBHOOK_SECRET ||= "";
 
 // Swish
-export const Swish_Payee_Number = process.env.SWISH_PAYEE_NUMBER ?? "";
+export const Swish_Payee_Number = process.env.SWISH_PAYEE_NUMBER ||= "";
 
 // Paypal
-export const Paypal_Client_Id = process.env.PAYPAL_CLIENT_ID ?? "";
-export const Paypal_Client_Secret = process.env.PAYPAL_CLIENT_SECRET ?? "";
+export const Paypal_Client_Id = process.env.PAYPAL_CLIENT_ID ||= "";
+export const Paypal_Client_Secret = process.env.PAYPAL_CLIENT_SECRET ||= "";
 
 // CPG stuff..
 export const CPG_Customer_Panel_Domain = process.env.CPG_CUSTOMER_PANEL_DOMAIN;
